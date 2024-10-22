760f814ff5d081b28fecaf7ff4d466eca77e8b36
minyeong <minyk0512@gmail.com>
2024-10-22 17:06:29 +0900

    remove: 네비게이션에서 로그아웃 제거

--- src/components/layouts/Navigation.tsx
+++ src/components/layouts/Navigation.tsx
@@ -1,19 +1,13 @@
 'use client';
 import Link from 'next/link';
-import { createClient } from '@/utils/supabase/client';
-import { useRouter } from 'next/navigation';
-import useSignOut from '@/hooks/auth/useSignOut';
 
 const Navigation = ({ isAuthenticated }: { isAuthenticated: boolean }) => {
-  const { handleSignOut } = useSignOut();
-
   return (
     <div>
       <Link href='/review'>후기</Link>
       {isAuthenticated ? (
         <>
           <Link href='/mypage'>마이페이지</Link>
-          <button onClick={handleSignOut}>로그아웃</button>
         </>
       ) : (
         <Link href='/signin'>로그인</Link>
