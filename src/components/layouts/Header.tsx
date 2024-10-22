54ad8f16be5271d10743727afa9eeda247302079
minyeong <minyk0512@gmail.com>
2024-10-23 02:17:43 +0900

    fix: 로그인 여부 확인용 메서드 변경

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,16 +1,9 @@
-import { createClient } from '@/utils/supabase/server';
+import { getIsLogin } from '@/utils/supabase/server';
 import Navigation from '@/components/layouts/Navigation';
 import Link from 'next/link';
-import { UserData } from '@/types/supabaseMethod.types';
 
 const Header = async () => {
-  const client = createClient();
-  const { data }: UserData = await client.auth.getUser();
-
-  let isAuthenticated: boolean = false;
-  if (data.user) {
-    isAuthenticated = true;
-  }
+  const isAuthenticated = await getIsLogin();
 
   return (
     <header className='w-full h-[64px] bg-gray-400 flex justify-around items-center'>
