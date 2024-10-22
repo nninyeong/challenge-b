f9b227a6caa56a8ebbbc2355add14750222ee077
sHyunis <162306191+sHyunis@users.noreply.github.com>
2024-10-22 21:16:07 +0900

    refactor:mypage클라이언트에서 서버사이드로 변경

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,7 +1,6 @@
-'use client';
-
 import Image from 'next/image';
 import defaultImage from '@/assets/images/defaultImg.jpg';
+import { createClient, getIsLogin } from '@/utils/supabase/server';
 import { SiTinyletter } from 'react-icons/si';
 import { TiExport } from 'react-icons/ti';
 import { FaFileDownload } from 'react-icons/fa';
@@ -9,56 +8,36 @@ import { AiOutlineShoppingCart } from 'react-icons/ai';
 import { MdOutlineQuestionAnswer } from 'react-icons/md';
 import { GoHeartFill } from 'react-icons/go';
 import { MdRateReview } from 'react-icons/md';
-import { useQuery } from '@tanstack/react-query';
-import { getUserInfoApi } from '@/utils/getUserInfoApi';
-import { User } from '@supabase/supabase-js';
+import { redirect } from 'next/navigation';
 
-const MyPage = () => {
+const MyPage = async (): Promise<JSX.Element | null> => {
+  const isLogin = await getIsLogin();
+  if (!isLogin) {
+    redirect('/signin');
+  }
+
+  const serverClient = createClient();
   const {
-    data: userInfo,
-    isLoading,
-    error,
-  } = useQuery<User | null>({
-    queryKey: ['userInfo'],
-    queryFn: getUserInfoApi,
-  });
+    data: { user },
+  } = await serverClient.auth.getUser();
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-  if (error) {
-    return <div>Error : {error?.message}</div>;
-  }
-  const profileUrl = userInfo?.user_metadata.avatar_url;
-
-  // const handleLogout = async()=>{
-  //   const logout = await
-  // }
+  const profileUrl = user?.user_metadata?.avatar_url || defaultImage;
 
   return (
     <div className='w-[375px] mx-auto bg-gray-700'>
       <div className='w-[90%] mx-auto pt-1 pb-1'>
-        {userInfo ? (
+        {user ? (
           <div className='flex gap-8 items-center mt-12'>
-            <div className='rounded '>
-              {profileUrl ? (
-                <Image
-                  src={profileUrl}
-                  width={100}
-                  height={100}
-                  alt='profileImage'
-                  style={{ borderRadius: '50%' }}
-                />
-              ) : (
-                <Image
-                  src={defaultImage}
-                  width={100}
-                  height={100}
-                  alt='profileImage'
-                />
-              )}
+            <div className='rounded'>
+              <Image
+                src={profileUrl}
+                width={100}
+                height={100}
+                alt='profileImage'
+                style={{ borderRadius: '50%' }}
+              />
             </div>
-            <h2 className='font-bold text-2xl'>{userInfo.user_metadata?.full_name}</h2>
+            <h2 className='font-bold text-2xl'>{user?.user_metadata?.full_name}</h2>
           </div>
         ) : (
           <div>정보가 없습니다</div>
@@ -74,7 +53,7 @@ const MyPage = () => {
             </div>
           ))}
         </div>
-        <nav className='mt-12 '>
+        <nav className='mt-12'>
           {MENULISTS.map((menulist) => (
             <ul
               key={menulist.menu}
@@ -85,10 +64,7 @@ const MyPage = () => {
             </ul>
           ))}
         </nav>
-        <div
-          // onClick={handleLogout()}
-          className='w-[full] bg-gray-400 text-black font-bold p-4 mt-12 mb-12 rounded text-center cursor-pointer'
-        >
+        <div className='w-full bg-gray-400 text-black font-bold p-4 mt-12 mb-12 rounded text-center cursor-pointer'>
           로그아웃
         </div>
       </div>
@@ -99,35 +75,14 @@ const MyPage = () => {
 export default MyPage;
 
 const MENUCARDS = [
-  {
-    icon: <SiTinyletter size={50} />,
-    menu: '내 청첩장 보기',
-  },
-  {
-    icon: <TiExport size={50} />,
-    menu: '청첩장 공유하기',
-  },
-  {
-    icon: <FaFileDownload size={50} />,
-    menu: '방문객 명단 다운로드',
-  },
+  { icon: <SiTinyletter size={50} />, menu: '내 청첩장 보기' },
+  { icon: <TiExport size={50} />, menu: '청첩장 공유하기' },
+  { icon: <FaFileDownload size={50} />, menu: '방문객 명단 다운로드' },
 ];
 
 const MENULISTS = [
-  {
-    icon: <AiOutlineShoppingCart size={20} />,
-    menu: '결재내역',
-  },
-  {
-    icon: <MdOutlineQuestionAnswer size={20} />,
-    menu: '1:1 문의',
-  },
-  {
-    icon: <GoHeartFill size={20} />,
-    menu: '내 찜리스트',
-  },
-  {
-    icon: <MdRateReview size={20} />,
-    menu: '나의 후기 관리',
-  },
+  { icon: <AiOutlineShoppingCart size={20} />, menu: '결재내역' },
+  { icon: <MdOutlineQuestionAnswer size={20} />, menu: '1:1 문의' },
+  { icon: <GoHeartFill size={20} />, menu: '내 찜리스트' },
+  { icon: <MdRateReview size={20} />, menu: '나의 후기 관리' },
 ];
--- src/utils/getUserInfoApi.ts
+++ src/utils/getUserInfoApi.ts
@@ -2,9 +2,14 @@ import { User } from '@supabase/supabase-js';
 import browserClient from './supabase/client';
 
 export const getUserInfoApi = async (): Promise<User | null> => {
-  const { data, error } = await browserClient.auth.getSession();
-  if (error) {
-    console.log('Mypage getUser error', error.message);
+  try {
+    const { data, error } = await browserClient.auth.getSession();
+    if (error) {
+      console.error('getUserInfoError', error);
+    }
+    return data.session?.user || null;
+  } catch (error) {
+    console.log('Mypage getUser error', error);
     return null;
   }
 
