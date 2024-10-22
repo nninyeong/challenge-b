17a2d3e56b6fc22f660c5374fe986bc74d8175b4
minyeong <minyk0512@gmail.com>
2024-10-22 15:35:18 +0900

    feat: 카카오로그인 추가

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,21 +1,30 @@
 'use client';
 
 import { createClient } from '@/utils/supabase/client';
-
 const SignInPage = () => {
   const client = createClient();
-
   const handleGoogleSignIn = () => {
     client.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: 'http://localhost:3000/auth/callback',
+        redirectTo: window.origin + '/auth/callback',
       },
     });
   };
+
+  const handleKakaoSignIn = async () => {
+    await client.auth.signInWithOAuth({
+      provider: 'kakao',
+      options: {
+        redirectTo: window.origin + '/auth/callback',
+      },
+    });
+  };
+
   return (
     <div>
       <button onClick={handleGoogleSignIn}>구글 로그인</button>
+      <button onClick={handleKakaoSignIn}>카카오 로그인</button>
     </div>
   );
 };
