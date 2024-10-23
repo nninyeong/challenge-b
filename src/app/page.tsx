863d2204136b7d6fd12173d8c4590ea95c5f9d9f
Subin, Han <ella0411@naver.com>
2024-10-23 11:05:07 +0900

    add: 리뷰 캐러셀 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+import Carousel from '@/components/main/Carousel';
+
 export default async function Home() {
-  return <div></div>;
+  return (
+    <div className='flex flex-col gap-[40px]'>
+      <div className='flex'>
+        <div>
+          <h1 className='text-[20px]'>서비스명 이용 후기</h1>
+          <p className='text-[14px]'>To Speed up Your Creative Woreflow</p>
+        </div>
+        <button className='text-[14px]'>바로가기</button>
+      </div>
+      <Carousel />
+    </div>
+  );
 }
