2bcc903699e3e254ed7806dcf33f70491e168228
GiSeong Lee <cj8928@gmail.com>
2024-10-24 20:32:30 +0900

    style: 폼 스타일 설정

--- src/app/create/card/page.tsx
+++ src/app/create/card/page.tsx
@@ -1,22 +1,33 @@
 'use client';
+import AccountInput from '@/components/create/AccountInput';
+import AccountPreView from '@/components/create/preview/AccountPreView';
+import { AccountInfoType } from '@/types/accountType.type';
 import { useEffect, useRef, useState } from 'react';
 import { Control, FormProvider, useForm, useFormContext, useWatch } from 'react-hook-form';
+import { MdNavigateBefore, MdNavigateNext } from 'react-icons/md';
 
-const Test1Watch = ({ control }: { control: Control<{ test1: string; test2: string; test3: string }> }) => {
+export type FormType = {
+  test1: string;
+  test2: string;
+  test3: string;
+  account: AccountInfoType;
+};
+
+const Test1Watch = ({ control }: { control: Control<FormType> }) => {
   const test1Watch = useWatch({
     control,
     name: 'test1',
   });
   return <p>Watch: {test1Watch}</p>;
 };
-const Test2Watch = ({ control }: { control: Control<{ test1: string; test2: string; test3: string }> }) => {
+const Test2Watch = ({ control }: { control: Control<FormType> }) => {
   const test2Watch = useWatch({
     control,
     name: 'test2',
   });
   return <p>Watch: {test2Watch}</p>;
 };
-const Test3Watch = ({ control }: { control: Control<{ test1: string; test2: string; test3: string }> }) => {
+const Test3Watch = ({ control }: { control: Control<FormType> }) => {
   const test3Watch = useWatch({
     control,
     name: 'test3',
@@ -25,10 +36,33 @@ const Test3Watch = ({ control }: { control: Control<{ test1: string; test2: stri
 };
 
 const CreateCardPage = () => {
-  const methods = useForm({ mode: 'onChange', defaultValues: { test1: '', test2: '', test3: '' } });
-  const onSubmit = (data: { test1: string; test2: string; test3: string }) => console.log(data);
+  const methods = useForm({
+    mode: 'onChange',
+    defaultValues: {
+      test1: '',
+      test2: '',
+      test3: '',
+      account: {
+        title: '',
+        content: '',
+        bride: [
+          { bank: '', accountNumber: '', depositor: '' },
+          { bank: '', accountNumber: '', depositor: '' },
+          { bank: '', accountNumber: '', depositor: '' },
+        ],
+        groom: [
+          { bank: '', accountNumber: '', depositor: '' },
+          { bank: '', accountNumber: '', depositor: '' },
+          { bank: '', accountNumber: '', depositor: '' },
+        ],
+      },
+    },
+  });
+
+  const onSubmit = (data: FormType) => console.log(data);
   const [currentStep, setCurrentStep] = useState(1);
   const refs = [
+    useRef<HTMLDivElement | null>(null),
     useRef<HTMLDivElement | null>(null),
     useRef<HTMLDivElement | null>(null),
     useRef<HTMLDivElement | null>(null),
@@ -67,7 +101,7 @@ const CreateCardPage = () => {
         if (entry.isIntersecting) {
           const stepIndex = refs.findIndex((ref) => ref.current === entry.target);
           if (stepIndex !== -1) {
-            setCurrentStep(stepIndex + 1); // Update the step based on visible section
+            setCurrentStep(stepIndex + 1);
           }
         }
       });
@@ -91,25 +125,7 @@ const CreateCardPage = () => {
   }, [refs]);
   console.log(currentStep);
   return (
-    <div className='relative'>
-      <div className='fixed bottom-0 w-full'>
-        <button
-          type='button'
-          onClick={handlePrevious}
-          className='bg-red-300'
-          disabled={currentStep === 1}
-        >
-          Previous
-        </button>
-        <button
-          className='bg-blue-300'
-          type='button'
-          onClick={handleNext}
-          disabled={currentStep === refs.length}
-        >
-          Next
-        </button>
-      </div>
+    <div className='relative w-full h-full'>
       <div
         style={{ minHeight: 'calc(100vh - 114px)' }}
         ref={refs[0]}
@@ -132,15 +148,36 @@ const CreateCardPage = () => {
         <Test3Watch control={methods.control} />
       </div>
       <div
-        className='overflow-auto'
-        style={{ height: 'calc(100vh - 114px)', paddingBottom: '60px' }}
+        style={{ minHeight: 'calc(100vh - 114px)' }}
+        ref={refs[3]}
       >
-        {/* 현재 단계에 맞는 컴포넌트 및 입력 필드 */}
+        <AccountPreView control={methods.control} />
+      </div>
+
+      <div className='fixed bottom-0 left-0 right-0 px-4'>
         <FormProvider {...methods}>
           <form
+            className='bg-[#bfbfbf] bg-opacity-50 px-4 rounded-lg h-[320px]'
             onSubmit={methods.handleSubmit(onSubmit)}
-            className='fixed bottom-5 right-0'
           >
+            <div className='w-full flex items-center justify-end'>
+              <button
+                type='button'
+                onClick={handlePrevious}
+                className='bg-red-300'
+                disabled={currentStep === 1}
+              >
+                <MdNavigateBefore />
+              </button>
+              <button
+                className='bg-blue-300'
+                type='button'
+                onClick={handleNext}
+                disabled={currentStep === refs.length}
+              >
+                <MdNavigateNext />
+              </button>
+            </div>
             <div style={{ display: currentStep === 1 ? 'block' : 'none' }}>
               <InputTest1 />
             </div>
@@ -150,8 +187,11 @@ const CreateCardPage = () => {
             <div style={{ display: currentStep === 3 ? 'block' : 'none' }}>
               <InputTest3 />
             </div>
+            <div style={{ display: currentStep === 4 ? 'block' : 'none' }}>
+              <AccountInput />
+            </div>
             <button
-              style={{ display: currentStep === 3 ? 'block' : 'none' }}
+              style={{ display: currentStep === 4 ? 'block' : 'none' }}
               type='submit'
             >
               제출
