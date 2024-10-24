3aa980eff98005651e0c9d93630e9eec0fdd6688
minyeong <minyk0512@gmail.com>
2024-10-25 03:28:35 +0900

    style: 미리보기 영역 배치 정렬

--- src/components/create/preview/WeddingInfoPreView.tsx
+++ src/components/create/preview/WeddingInfoPreView.tsx
@@ -10,12 +10,12 @@ const WeddingInfoPreView = ({ control }: { control: Control<InvitationFormType> 
   return (
     <div>
       웨딩 정보 입력 미리보기 영역
-      {weddingInfoWatch?.date}
-      {weddingInfoWatch.time.hour}
-      {weddingInfoWatch.time.minute}
-      {weddingInfoWatch.weddingHallAddress}
-      {weddingInfoWatch.weddingHallName}
-      {weddingInfoWatch.weddingHallContact}
+      <p>날짜: {weddingInfoWatch?.date}</p>
+      <p>{weddingInfoWatch.time.hour}시</p>
+      <p>{weddingInfoWatch.time.minute}분</p>
+      <p>예식장 주소: {weddingInfoWatch.weddingHallAddress}</p>
+      <p>예식장 이름: {weddingInfoWatch.weddingHallName}</p>
+      <p>연락처: {weddingInfoWatch.weddingHallContact}</p>
     </div>
   );
 };
