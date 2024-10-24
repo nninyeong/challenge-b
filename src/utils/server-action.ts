e2988f1ea44cadf208f6b3b4adfc0f06366b170b
GiSeong Lee <cj8928@gmail.com>
2024-10-24 09:36:36 +0900

    fix: server action try/catch문 제거

--- src/utils/server-action.ts
+++ src/utils/server-action.ts
@@ -4,22 +4,18 @@ import { createClient } from './supabase/server';
 
 export const getReviewDetail = async (id: string) => {
   const serverClient = createClient();
-  try {
-    const { data, error } = await serverClient.from('reviews').select('*').eq('id', id);
-    if (error) {
-      throw new Error(`에러!: ${error}`);
-    }
-    return data[0];
-  } catch (error) {
-    console.error(error);
+  const { data, error } = await serverClient.from('reviews').select('*').eq('id', id);
+  if (error) {
+    throw new Error(`에러!: ${error}`);
   }
+  return data[0];
 };
 
 export const getUserInfo = async () => {
   const serverClient = createClient();
   const { data, error } = await serverClient.auth.getUser();
   if (error || !data?.user) {
-    redirect('/signin');
+    return redirect('/signin');
   }
   return data;
 };
