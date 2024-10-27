b26d63be070d44cd3e2e02b188c099f81a78fa61
lukby2457 <kjh7442455@gmail.com>
2024-10-28 01:37:09 +0900

    fix: 예식 날짜가 입력되지 않았을 시 D-000으로 표시 가능하게 수정

--- src/components/ddaycounter/DdayCounter.tsx
+++ src/components/ddaycounter/DdayCounter.tsx
@@ -2,12 +2,12 @@ const DdayCounter = ({ weddingInfoDate }: { weddingInfoDate: string }) => {
   const targetDate = new Date(weddingInfoDate);
   const currentDate = new Date();
   const diffInMs = targetDate.getTime() - currentDate.getTime();
-  const diffInDays = Math.ceil(diffInMs / (1000 * 60 * 60 * 24)).toLocaleString();
+  const diffInDays = isNaN(diffInMs) ? '000' : Math.ceil(diffInMs / (1000 * 60 * 60 * 24)).toLocaleString();
   return (
     <>
-      <div>{diffInDays === "0" ? "결혼식이 오늘 입니다." : "결혼까지 D-" + diffInDays + "일 남았습니다."}</div>
+      <div>{diffInDays === '0' ? '결혼식이 오늘 입니다.' : '결혼까지 D-' + diffInDays + '일 남았습니다.'}</div>
     </>
-  )
-}
+  );
+};
 
-export default DdayCounter
+export default DdayCounter;
