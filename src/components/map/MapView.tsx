75a94c568f4df90fd3e1bf4ff448588572d796ec
lukby2457 <kjh7442455@gmail.com>
2024-10-27 23:37:40 +0900

    fix: useState, useEffect를 별도의 hook으로 분리

--- src/components/map/MapView.tsx
+++ src/components/map/MapView.tsx
@@ -1,27 +1,10 @@
 "use client"
 
-import { useEffect, useState } from 'react';
+import useLatLng from '@/hooks/map/useLatLng';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
 
 const MapView = ({ address }: { address: string }) => {
-  const [coords, setCoords] = useState<{ lat: number; lng: number }>({ lat: 33.450701, lng: 126.570667 });
-
-  useEffect(() => {
-    if (address) {
-      kakao.maps.load(() => {
-        const geocoder = new kakao.maps.services.Geocoder();
-        geocoder.addressSearch(address, (res, status) => {
-          if (status === kakao.maps.services.Status.OK) {
-            const result = res[0];
-            setCoords({ lat: parseFloat(result.y), lng: parseFloat(result.x) });
-          } else {
-            alert('유효한 주소가 아닙니다. 다시 확인해주세요.');
-            setCoords({ lat: 33.450701, lng: 126.570667 });
-          }
-        });
-      });
-    }
-  }, [address]);
+  const coords = useLatLng(address);
 
   return (
     <div className='w-full px-4'>
