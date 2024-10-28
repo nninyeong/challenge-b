e3f01f0576e05c5ac4f7646469c841cda9b5ac95
lukby2457 <kjh7442455@gmail.com>
2024-10-28 22:45:36 +0900

    fix: useLatLng의 useEffect 함수 분리

--- src/hooks/map/useLatLng.ts
+++ src/hooks/map/useLatLng.ts
@@ -1,23 +1,27 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 const useLatLng = (address: string) => {
   const [coords, setCoords] = useState<{ lat: number; lng: number }>({ lat: 33.450701, lng: 126.570667 });
 
-  useEffect(() => {
-    if (address) {
-      kakao.maps.load(() => {
-        const geocoder = new kakao.maps.services.Geocoder();
-        geocoder.addressSearch(address, (res, status) => {
-          if (status === kakao.maps.services.Status.OK) {
-            const result = res[0];
-            setCoords({ lat: parseFloat(result.y), lng: parseFloat(result.x) });
-          } else {
-            alert('유효한 주소가 아닙니다. 다시 확인해주세요.');
-            setCoords({ lat: 33.450701, lng: 126.570667 });
-          }
-        });
+  const fetchCoordinates = useCallback(() => {
+    if (!address) return;
+
+    kakao.maps.load(() => {
+      const geocoder = new kakao.maps.services.Geocoder();
+      geocoder.addressSearch(address, (res, status) => {
+        if (status === kakao.maps.services.Status.OK) {
+          const result = res[0];
+          setCoords({ lat: parseFloat(result.y), lng: parseFloat(result.x) });
+        } else {
+          alert('유효한 주소가 아닙니다. 다시 확인해주세요.');
+          setCoords({ lat: 33.450701, lng: 126.570667 });
+        }
       });
-    }
+    });
+  }, [address]);
+
+  useEffect(() => {
+    fetchCoordinates();
   }, [address]);
 
   return coords;
