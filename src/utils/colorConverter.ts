595cf213186e4d52f99f98e767dcdcb7da4cc5e2
GiSeong Lee <cj8928@gmail.com>
2024-10-29 23:55:01 +0900

    delete: console.log()제거

--- src/utils/colorConverter.ts
+++ src/utils/colorConverter.ts
@@ -1,7 +1,6 @@
 import { ColorType } from '@/types/invitationFormType.type';
 
 const colorConverter = (color: ColorType) => {
-  console.log(color);
   return `rgba(${color.r},${color.g},${color.b},${color.a})`;
 };
 
