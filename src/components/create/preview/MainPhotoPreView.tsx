b90edcb7134a06b963dfa3a96f29e11ba1542bc6
GiSeong Lee <cj8928@gmail.com>
2024-10-29 23:54:38 +0900

    feat: 꾸미기 타입별 사진 꾸미기 구현

--- src/components/create/preview/MainPhotoPreView.tsx
+++ src/components/create/preview/MainPhotoPreView.tsx
@@ -2,7 +2,6 @@ import { InvitationFormType } from '@/types/invitationFormType.type';
 import Image from 'next/image';
 import { Control, useWatch } from 'react-hook-form';
 import { ArchSvg, EllipseSvg } from '../CustomSVG';
-import colorConverter from '@/utils/colorConverter';
 
 const MainPhotoPreView = ({ control }: { control: Control<InvitationFormType> }) => {
   const mainPhotoInfo = useWatch({
@@ -15,6 +14,11 @@ const MainPhotoPreView = ({ control }: { control: Control<InvitationFormType> })
     name: 'bgColor',
   });
 
+  const mainViewType = useWatch({
+    control,
+    name: 'mainView',
+  });
+
   return (
     <div className='w-full flex flex-col justify-center item-center mx-auto text-center text-black'>
       <div
@@ -23,21 +27,23 @@ const MainPhotoPreView = ({ control }: { control: Control<InvitationFormType> })
           __html: mainPhotoInfo?.introduceContent || '대표문구를 작성해주세요',
         }}
       />
-      <div className='flex justify-center items-center'>
+      <div
+        className={`flex justify-center items-center w-full ${mainViewType.type === 'fill' ? 'px-0' : 'px-[20px]'} `}
+      >
         {!mainPhotoInfo?.imageUrl ? (
           <p className='text-gray-500 w-[375px] h-[728px] bg-gray text-center'>이미지가 업로드되지 않았습니다.</p>
         ) : (
-          <div className='flex justify-center items-center relative w-[375px] h-[600px]'>
+          <div className='flex justify-center items-center relative w-full h-[600px]'>
             <Image
               src={mainPhotoInfo.imageUrl}
               alt='mainImg'
               objectFit='cover'
               fill
-              className='z-0 rounded-md'
+              className='z-0'
             />
             <div className='absolute inset-0 flex justify-center items-center'>
-              <ArchSvg color={svgBgColor} />
-              {/* <EllipseSvg color={svgBgColor} /> */}
+              {mainViewType.type === 'arch' && <ArchSvg color={svgBgColor} />}
+              {mainViewType.type === 'ellipse' && <EllipseSvg color={svgBgColor} />}
             </div>
           </div>
         )}
