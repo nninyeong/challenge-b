6eb66603c75fea61a682ad257c95b8757970f3ab
minyeong <minyk0512@gmail.com>
2024-10-30 15:21:36 +0900

    feat: 청첩장 제작 페이지로 제작 버튼 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 import ReviewsCarousel from '@/components/main/ReviewsCarousel';
 import { handleAuthCallback } from '@/utils/auth/authCallbackHandler';
+import LinkToCreateCard from '@/components/ui/LinkToCreateCard';
 
 export default function Home() {
   const router = useRouter();
@@ -11,5 +12,13 @@ export default function Home() {
     handleAuthCallback(router);
   }, []);
 
-  return <ReviewsCarousel />;
+  return (
+    <div className='items-center px-[16px]'>
+      <div className='relative bg-gray-300 w-[343px] h-[328px] mx-auto'>
+        소개화면 영역
+        <LinkToCreateCard buttonStyle='absolute right-[8px] bottom-[8px] w-[151px] h-[48px] text-[16px]' />
+      </div>
+      <ReviewsCarousel />
+    </div>
+  );
 }
