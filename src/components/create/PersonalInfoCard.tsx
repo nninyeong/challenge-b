b35961cc7f4b0dac0a4d8f47dea4461d035c1b8c
Subin, Han <ella0411@naver.com>
2024-10-30 21:16:25 +0900

    fix: 번호 클릭 처리

--- src/components/create/PersonalInfoCard.tsx
+++ src/components/create/PersonalInfoCard.tsx
@@ -3,28 +3,41 @@ import { IoIosCall } from 'react-icons/io';
 
 type PersonInfoCardProps = {
   label: string | null;
-  name: string;
-  phoneNumber: string;
+  name: string | null;
+  phoneNumber: string | null;
 };
 
 const PersonalInfoCard = ({ label, name, phoneNumber }: PersonInfoCardProps) => {
+  const defaultName = '이름';
+  const defaultLabel = '관계';
+  const defaultPhoneNumber = '번호를 입력하세요';
+
+  const handlePhoneClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    if (!phoneNumber) {
+      e.preventDefault();
+      alert(defaultPhoneNumber);
+    }
+  };
+
   return (
     <div className='flex flex-col justify-center items-center gap-2'>
-      <div className='flex  items-center gap-2'>
-        {label && <p className='text-center'>{label}</p>}
-        <p className='text-xl font-semibold'>{name ? name : '이름'}</p>
+      <div className='flex items-center gap-2'>
+        <p className='text-center'>{label || defaultLabel}</p>
+        <p className='text-xl font-semibold'>{name || defaultName}</p>
       </div>
 
       <div className='flex gap-5'>
         <a
           href={`tel:${phoneNumber}`}
           className='flex justify-center items-center relative rounded-t-lg overflow-hidden'
+          onClick={handlePhoneClick}
         >
           <IoIosCall className='text-2xl' />
         </a>
         <a
           href={`sms:${phoneNumber}`}
           className='flex justify-center items-center relative rounded-t-lg overflow-hidden'
+          onClick={handlePhoneClick}
         >
           <CiMail className='text-2xl' />
         </a>
