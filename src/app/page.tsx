6c2f7969d0705ce2b8e80845e2e2c681ad48ef38
Subin, Han <ella0411@naver.com>
2024-10-31 02:34:59 +0900

    fix: 배치 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   return (
     <div className='items-center px-[16px] h-full'>
       <FixedQuickLink />
-      <div className='relative bg-gray-300 w-[343px] h-[328px] mx-auto'>
+      <div className='relative bg-gray-300 w-[343px] h-[328px] mx-auto mb-[56px]'>
         소개화면 영역
         <LinkToCreateCard buttonStyle='absolute right-[8px] bottom-[8px]' />
       </div>
--- src/components/main/Gallery.tsx
+++ src/components/main/Gallery.tsx
@@ -5,30 +5,27 @@ const Gallery = () => {
   const images = Array(14).fill(placeholderImg);
 
   return (
-    <div className='flex flex-col gap-[40px]'>
+    <div className='flex flex-col gap-[40px] mb-[56px]'>
       <div>
         <h1 className='text-[20px] font-semibold'>드림카드 갤러리</h1>
         <p className='text-[14px]'>드림카드 모바일 청첩장 디자인을 둘러보세요.</p>
       </div>
-      {/* 가로 스크롤을 위한 외부 컨테이너 */}
-      <div className='overflow-x-auto'>
-        <div className='grid grid-cols-7 gap-4 w-full min-w-[375px]'>
-          {images.map((image, index) => {
-            return (
-              <div
-                key={`이미지${index}`}
-                className='relative w-[152px] h-[200px] rounded-lg overflow-hidden flex-shrink-0'
-              >
-                <Image
-                  src={image}
-                  alt={`디자인 이미지${index}`}
-                  layout='fill'
-                  objectFit='cover'
-                  priority
-                />
-              </div>
-            );
-          })}
+      <div className='flex justify-around overflow-x-auto'>
+        <div className='grid grid-rows-2 grid-flow-col gap-4 h-[416px] max-w-[1440px]'>
+          {images.map((image, index) => (
+            <div
+              key={`이미지${index}`}
+              className='relative w-[152px] h-[200px] rounded-lg overflow-hidden'
+            >
+              <Image
+                src={image}
+                alt={`디자인 이미지${index}`}
+                layout='fill'
+                objectFit='cover'
+                priority
+              />
+            </div>
+          ))}
         </div>
       </div>
     </div>
