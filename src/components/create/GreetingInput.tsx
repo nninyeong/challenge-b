b5c9467d3365e59e2bccd014c06a709ac3825952
sHyunis <162306191+sHyunis@users.noreply.github.com>
2024-10-30 14:54:45 +0900

    fix:useWatch, onChange, onClick내부 함수수정

--- src/components/create/GreetingInput.tsx
+++ src/components/create/GreetingInput.tsx
@@ -1,11 +1,14 @@
 'use client';
 
-import { useFormContext } from 'react-hook-form';
+import { useFormContext, useWatch } from 'react-hook-form';
 import TextEditor from './TextEditor';
 
 const GreetingInput = () => {
-  const { register, watch, setValue } = useFormContext();
-  const greetingContext = watch('greetingMessage.content');
+  const { register, setValue } = useFormContext();
+  const greetingContext = useWatch({ name: 'greetingMessage.content' });
+  const handleSetContent = (value: string) => {
+    setValue('greetingMessage.content', value);
+  };
   return (
     <div className='flex flex-col justify-center items-center gap-2'>
       <p className='font-bold text-black text-[14px] text-left'>인사말</p>
@@ -22,7 +25,7 @@ const GreetingInput = () => {
         <TextEditor
           placeholder='저희 두 사람의 작은 만남이 사랑의 결실을 이루어 소중한 결혼식을 올리게 되었습니다.평생 서로 귀하게 여기며 첫 마음 그대로 존중하며 살겠습니다.오로지 믿음과 사랑을 약속하는 날 오셔서 축복해 주시면 더 없는 기쁨으로 간직하겠습니다.'
           value={greetingContext || ''}
-          onChange={(value) => setValue('greetingMessage.content', value)}
+          onChange={handleSetContent}
           style={{ width: '266px', height: '180px' }}
         />
       </div>
