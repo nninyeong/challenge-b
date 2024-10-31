7ab638eedbf9149b05f4a3fc98edea5e1f351d82
minyeong <minyk0512@gmail.com>
2024-10-31 23:51:11 +0900

    feat: 공개 여부(isPrivate), 테이블에 청첩장 데이터 존재 여부에따라 안내문구 or notFound 연결되도록 설정

--- src/app/card/[id]/page.tsx
+++ src/app/card/[id]/page.tsx
@@ -1,5 +1,27 @@
-const CardPage = () => {
-  return <div></div>;
+import { notFound } from 'next/navigation';
+import { supabase } from '@/utils/supabase/createClient';
+
+export const generateStaticParams = async () => {
+  const { data } = await supabase.from('invitation').select('id');
+  return (
+    data?.map((invitation) => ({
+      id: invitation.id,
+    })) || []
+  );
+};
+
+const fetchInvitationData = async (id: string) => {
+  const { data, error } = await supabase.from('invitation').select('*').eq('id', id).single();
+
+  if (error || !data) notFound();
+
+  return data;
+};
+
+const CardPage = async ({ params }: { params: { id: string } }) => {
+  const { isPrivate } = await fetchInvitationData(params.id);
+
+  return <div>{isPrivate ? <div>아직 공개되지 않은 청첩장입니다.</div> : <div>청첩장 내용</div>}</div>;
 };
 
 export default CardPage;
