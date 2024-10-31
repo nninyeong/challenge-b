53ed3c896b95dc3f0e55149de86bd0e2a6f4f0fb
sHyunis <162306191+sHyunis@users.noreply.github.com>
2024-11-01 05:06:30 +0900

    fix:isPrivate여부변경

--- src/components/mypage/TogglePrivate.tsx
+++ src/components/mypage/TogglePrivate.tsx
@@ -45,10 +45,10 @@ const TogglePrivate = () => {
   return (
     <div
       onClick={toggleSwitch}
-      className={`w-14 h-8 flex items-center rounded-full p-1 cursor-pointer ${isPrivate ? 'bg-primary-300' : 'bg-gray-400'}`}
+      className={`w-14 h-8 flex items-center rounded-full p-1 cursor-pointer ${!isPrivate ? 'bg-primary-300' : 'bg-gray-400'}`}
     >
       <div
-        className={`bg-white w-6 h-6 rounded-full shadow-md transform duration-300 ${isPrivate ? 'translate-x-6' : ''}`}
+        className={`bg-white w-6 h-6 rounded-full shadow-md transform duration-300 ${!isPrivate ? 'translate-x-6' : ''}`}
       ></div>
     </div>
   );
