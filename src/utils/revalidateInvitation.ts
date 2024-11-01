6bc25d2473102b2dbea487daa2e023e9ed2e7134
minyeong <minyk0512@gmail.com>
2024-11-01 16:48:26 +0900

    feat: 요청 성공 토스트메세지 추가

--- src/utils/revalidateInvitation.ts
+++ src/utils/revalidateInvitation.ts
@@ -1,10 +1,15 @@
+import { Notify } from 'notiflix';
+
 export const revalidateInvitation = async (invitationId: string) => {
   const response = await fetch(`/api/revalidate?id=${invitationId}`);
   const data = await response.json();
 
   if (response.ok) {
-    console.log('업데이트 성공');
-  } else {
-    return console.log(data.message || '업데이트 실패');
+    Notify.success('청첩장 공개가 전환되었습니다.');
+  }
+
+  if (!response.ok) {
+    console.error(data.message || '업데이트 실패');
+    throw new Error(data.message);
   }
 };
