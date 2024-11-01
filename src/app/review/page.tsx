de7748abb55b53f93a008461adabfba1736d072f
sHyunis <162306191+sHyunis@users.noreply.github.com>
2024-11-01 23:07:37 +0900

    refactor:무한스크롤 Intersection Observer 리펙토링

--- src/app/review/page.tsx
+++ src/app/review/page.tsx
@@ -1,41 +1,30 @@
 'use client';
-import { getReview } from '@/utils/getReview';
-import { useInfiniteQuery } from '@tanstack/react-query';
-import { useEffect } from 'react';
+
+import { useCallback, useRef } from 'react';
 import ReviewImage from '@/components/review/ReviewImage';
-import { Review } from '@/types/review.types';
 import ReviewCard from '@/components/review/ReviewCard';
+import { useReviewInfinite } from '@/hooks/queries/review/useGetReview';
 
 const ReviewPage = () => {
-  const row = 10;
+  const { data: reviewsData, isLoading, error, fetchNextPage, hasNextPage, isFetchingNextPage } = useReviewInfinite();
+  const observerRef = useRef<IntersectionObserver | null>(null);
 
-  const {
-    data: reviewsData,
-    isLoading,
-    error,
-    fetchNextPage,
-    hasNextPage,
-    isFetchingNextPage,
-  } = useInfiniteQuery<Review[]>({
-    queryKey: ['reviews'],
-    queryFn: async ({ pageParam }) => await getReview({ pageParam: pageParam as number, row }),
-    initialPageParam: 0,
-    getNextPageParam: (lastPage, allPages) => {
-      return lastPage.length === row ? allPages.length * row : undefined;
+  const lastReviewRef = useCallback(
+    (node: HTMLDivElement | null) => {
+      if (isFetchingNextPage) return;
+
+      if (observerRef.current) observerRef.current.disconnect();
+
+      observerRef.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasNextPage) {
+          fetchNextPage();
+        }
+      });
+
+      if (node) observerRef.current.observe(node);
     },
-  });
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-      if (scrollTop + clientHeight >= scrollHeight - 10 && hasNextPage && !isFetchingNextPage) {
-        fetchNextPage();
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, [hasNextPage, isFetchingNextPage, fetchNextPage]);
+    [isFetchingNextPage, fetchNextPage, hasNextPage],
+  );
 
   if (isLoading) return <div>로딩 중...</div>;
   if (error) return <div>에러가 발생했습니다: {error.message}. 다시 시도해 주세요.</div>;
@@ -46,9 +35,14 @@ const ReviewPage = () => {
     <div className='flex flex-col w-full p-4'>
       <h1>후기</h1>
       <ReviewImage />
-
       <div>
         <ReviewCard reviews={reviews} />
+        {reviews.length > 0 && (
+          <div
+            ref={lastReviewRef}
+            className='h-1'
+          />
+        )}
       </div>
       {isFetchingNextPage && <div>더 불러오는 중...</div>}
     </div>
--- src/hooks/queries/review/useGetReview.ts
+++ src/hooks/queries/review/useGetReview.ts
@@ -1,8 +1,9 @@
 'use client';
 
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useInfiniteQuery } from '@tanstack/react-query';
 import { QUERY_KEYS } from '../queryKeys';
-import { getAllImageReviews, getAuthUsersProfile } from '@/utils/getReview';
+import { getAllImageReviews, getAuthUsersProfile, getReview } from '@/utils/getReview';
+import { Review } from '@/types/review.types';
 
 export const useAuthUserQuery = () => {
   return useQuery({
@@ -15,5 +16,18 @@ export const useReviewImage = () => {
   return useQuery({
     queryKey: QUERY_KEYS.allImageReviews(),
     queryFn: getAllImageReviews,
+  });
+};
+
+export const useReviewInfinite = () => {
+  const row = 10;
+
+  return useInfiniteQuery<Review[]>({
+    queryKey: QUERY_KEYS.reviews(),
+    queryFn: async ({ pageParam }) => await getReview({ pageParam: pageParam as number, row }),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.length === row ? allPages.length * row : undefined;
+    },
   });
 };
