1ec1c00a3cdce45dc0bb2d4b82414b464bff83e9
minyeong <minyk0512@gmail.com>
2024-11-02 02:04:11 +0900

    refactor: 매핑된 컴포넌트 객체에서 해당하는 객체 반환

--- src/app/card/[id]/page.tsx
+++ src/app/card/[id]/page.tsx
@@ -1,15 +1,9 @@
 import { notFound } from 'next/navigation';
 import { supabase } from '@/utils/supabase/createClient';
 import { convertToCamelCase } from '@/utils/convert/invitaitonTypeConvert';
-import MainPhoto from '@/components/card/MainPhoto';
-import Greeting from '@/components/card/Greeting';
-import PersonalInfoOnSharedCard from '@/components/card/PersonalInfoOnSharedCard';
-import Account from '@/components/card/Account';
-import WeddingInfo from '@/components/card/WeddingInfo';
-import NavigationDetails from '@/components/card/NavigationDetails';
-import GuestInfo from '@/components/card/GuestInfo';
-import WeddingGallery from '@/components/card/WeddingGallery';
 import { COMPONENT_TYPES } from '@/constants/componentTypes';
+import { convertOrderToComponent } from '@/utils/convert/convertOrderToComponent';
+import { Fragment } from 'react';
 
 export const generateStaticParams = async () => {
   const { data } = await supabase.from('invitation').select('id');
@@ -30,26 +24,11 @@ const fetchInvitationData = async (id: string) => {
 
 const CardPage = async ({ params }: { params: { id: string } }) => {
   const invitation = await fetchInvitationData(params.id);
-  const {
-    gallery,
-    mainView,
-    bgColor,
-    stickers,
-    greetingMessage,
-    guestbook,
-    attendance,
-    personalInfo,
-    weddingInfo,
-    account,
-    navigationDetail,
-    dDay,
-    mainPhotoInfo,
-    isPrivate,
-  } = convertToCamelCase(invitation);
+  const { isPrivate, ...invitationData } = convertToCamelCase(invitation);
 
   const testOrderData = [
-    { order: 0, typeOnSharedCard: COMPONENT_TYPES.MAIN_PHOTO },
-    { order: 1, typeOnSharedCard: COMPONENT_TYPES.GREETING },
+    { order: 1, typeOnSharedCard: COMPONENT_TYPES.MAIN_PHOTO },
+    { order: 0, typeOnSharedCard: COMPONENT_TYPES.GREETING },
     { order: 2, typeOnSharedCard: COMPONENT_TYPES.PERSONAL_INFO },
     { order: 5, typeOnSharedCard: COMPONENT_TYPES.ACCOUNT },
     { order: 4, typeOnSharedCard: COMPONENT_TYPES.WEDDING_INFO },
@@ -58,46 +37,15 @@ const CardPage = async ({ params }: { params: { id: string } }) => {
     { order: 7, typeOnSharedCard: COMPONENT_TYPES.GALLERY },
   ];
 
-  const testComponentList = {
-    [COMPONENT_TYPES.MAIN_PHOTO]: (
-      <MainPhoto
-        mainPhotoInfo={mainPhotoInfo}
-        bgColor={bgColor}
-        mainView={mainView}
-        stickers={stickers}
-      />
-    ),
-    [COMPONENT_TYPES.GREETING]: <Greeting greetingMessage={greetingMessage} />,
-    [COMPONENT_TYPES.PERSONAL_INFO]: <PersonalInfoOnSharedCard personalInfo={personalInfo} />,
-    [COMPONENT_TYPES.ACCOUNT]: <Account account={account} />,
-    [COMPONENT_TYPES.WEDDING_INFO]: <WeddingInfo weddingInfo={weddingInfo} />,
-    [COMPONENT_TYPES.NAVIGATION_DETAILS]: (
-      <NavigationDetails
-        navigationDetail={navigationDetail}
-        weddingInfo={weddingInfo}
-      />
-    ),
-    [COMPONENT_TYPES.GUEST_INFO]: (
-      <GuestInfo
-        attendance={attendance}
-        guestbook={guestbook}
-        dDay={dDay}
-        weddingInfo={weddingInfo}
-      />
-    ),
-    [COMPONENT_TYPES.GALLERY]: <WeddingGallery gallery={gallery} />,
-  };
-
   return isPrivate ? (
     <div>아직 공개되지 않은 청첩장입니다.</div>
   ) : (
     <div>
       {testOrderData
         .sort((a, b) => a.order - b.order)
-        .map(({ typeOnSharedCard }, index) => {
-          const Component = testComponentList[typeOnSharedCard]; // typeOnSharedCard에 해당하는 컴포넌트 찾기
-          return Component ?? null;
-        })}
+        .map(({ typeOnSharedCard }, index) => (
+          <Fragment key={index}>{convertOrderToComponent(typeOnSharedCard, invitationData)}</Fragment>
+        ))}
     </div>
   );
 };
