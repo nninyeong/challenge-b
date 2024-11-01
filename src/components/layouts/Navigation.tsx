4fe5a9c7c587bda93f9367467b7b59231fd89862
minyeong <minyk0512@gmail.com>
2024-11-01 13:26:16 +0900

    fix: Notify 초기화 위치 변경

--- src/components/layouts/Navigation.tsx
+++ src/components/layouts/Navigation.tsx
@@ -4,10 +4,27 @@ import { useEffect, useState } from 'react';
 import { createClient } from '@/utils/supabase/client';
 import LinkToMypage from '@/components/ui/LinkToMypage';
 import LinkToReviewPage from '@/components/ui/LinkToReviewPage';
+import { Notify } from 'notiflix';
 
 const Navigation = ({ initialAuthState }: { initialAuthState: boolean }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(initialAuthState);
   const client = createClient();
+
+  useEffect(() => {
+    Notify.init({
+      backOverlay: false,
+      useIcon: false,
+      borderRadius: '20px',
+      position: 'center-bottom',
+      distance: '50px',
+      cssAnimationDuration: 200,
+      fontFamily: 'Main',
+      fontSize: '16px',
+      success: {
+        background: 'rgba(64, 64, 64, 0.8)',
+      },
+    });
+  }, []);
 
   useEffect(() => {
     const { data: authListener } = client.auth.onAuthStateChange((event) => {
