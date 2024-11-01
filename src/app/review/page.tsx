1ea72993cb86763f5b2eb1d70b649deb8df88f66
GiSeong Lee <cj8928@gmail.com>
2024-11-01 15:55:18 +0900

    feat: 후기 작성시 스크롤 불가 및 바텀시트 생성

--- src/app/review/page.tsx
+++ src/app/review/page.tsx
@@ -1,14 +1,22 @@
 'use client';
 import { getReview } from '@/utils/getReview';
 import { useInfiniteQuery } from '@tanstack/react-query';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import ReviewImage from '@/components/review/ReviewImage';
 import { Review } from '@/types/review.types';
 import ReviewCard from '@/components/review/ReviewCard';
+import ReviewWriteButton from '@/components/review/ReviewWriteButton';
+import ReviewWriteBottomSheet from '@/components/review/ReviewWriteBottomSheet';
+import { createPortal } from 'react-dom';
 
 const ReviewPage = () => {
   const row = 10;
+  const [portalElement, setPortalElement] = useState<Element | null>(null);
+  const [openBottomSheet, setOpenBottomSheet] = useState<boolean>(false);
 
+  useEffect(() => {
+    setPortalElement(document.getElementById('modal'));
+  }, []);
   const {
     data: reviewsData,
     isLoading,
@@ -37,20 +45,35 @@ const ReviewPage = () => {
     return () => window.removeEventListener('scroll', handleScroll);
   }, [hasNextPage, isFetchingNextPage, fetchNextPage]);
 
+  useEffect(() => {
+    if (openBottomSheet) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'unset';
+    }
+    return () => {
+      document.body.style.overflow = 'unset';
+    };
+  }, [openBottomSheet]);
+
   if (isLoading) return <div>로딩 중...</div>;
   if (error) return <div>에러가 발생했습니다: {error.message}. 다시 시도해 주세요.</div>;
 
   const reviews = reviewsData?.pages.flatMap((page) => page) || [];
 
   return (
-    <div className='flex flex-col w-full p-4'>
+    <div className={`flex flex-col w-full p-4 ${openBottomSheet && 'bg-black bg-opacity-60'}`}>
       <h1>후기</h1>
       <ReviewImage />
 
       <div>
         <ReviewCard reviews={reviews} />
       </div>
+      <ReviewWriteButton setOpenBottomSheet={setOpenBottomSheet} />
       {isFetchingNextPage && <div>더 불러오는 중...</div>}
+      {openBottomSheet && portalElement
+        ? createPortal(<ReviewWriteBottomSheet setOpenBottomSheet={setOpenBottomSheet} />, portalElement)
+        : null}
     </div>
   );
 };
