7f7d9f05c7fed2d4d74863f9a3f53f58c2e910c3
Subin, Han <ella0411@naver.com>
2024-11-01 11:09:32 +0900

    fix: mood -> mood_preset 변경

--- src/utils/convert/invitaitonTypeConvert.ts
+++ src/utils/convert/invitaitonTypeConvert.ts
@@ -18,7 +18,7 @@ export const convertToCamelCase = (invitation: Invitation): InvitationFormType =
   return {
     gallery: invitation.gallery as { images: [] },
     type: invitation.type as 'scroll' | 'slide',
-    moodPreset: invitation.mood as MoodPresetType,
+    moodPreset: invitation.mood_preset as MoodPresetType,
     mainView: invitation.main_view as DecorateImageType,
     bgColor: invitation.bg_color as ColorType,
     stickers: invitation.stickers as StickerType[],
@@ -41,7 +41,7 @@ export const convertToSnakeCase = (invitation: InvitationFormType) => {
   return {
     gallery: invitation.gallery,
     type: invitation.type,
-    mood: invitation.moodPreset,
+    mood_preset: invitation.moodPreset,
     main_view: invitation.mainView,
     bg_color: invitation.bgColor,
     stickers: invitation.stickers,
