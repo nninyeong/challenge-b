8b6c7b454a49a6195de739ebdfeb602bb27099df
lukby2457 <kjh7442455@gmail.com>
2024-11-02 16:54:32 +0900

    style: 수정된 textarea 디자인 반영

--- src/components/create/NavigationDetailInput.tsx
+++ src/components/create/NavigationDetailInput.tsx
@@ -25,7 +25,7 @@ const NavigationDetailInput = () => {
           <span className='ml-2'>지도 표시</span>
         </label>
       </div>
-      <div className='mb-8'>
+      <div className='mb-[41px]'>
         <label className='text-[14px] text-gray-600 flex items-center cursor-pointer'>
           <input
             type='checkbox'
@@ -46,20 +46,20 @@ const NavigationDetailInput = () => {
           <span className='text-[12px] text-gray-600'>(카카오맵, 티맵, 네이버)</span>
         </label>
       </div>
-      <div className='flex flex-col gap-3'>
-        <div className='flex justify-between w-full gap-4'>
-          <label className='flex justify-start items-center text-[14px] w-14 text-gray-700'>지하철</label>
-          <textarea
-            placeholder='오시는 길 내용을 입력하세요.'
-            {...register('navigationDetail.subway')}
-            className='h-[64px] w-full pl-[8px] py-[9px] border text-[12px] rounded-[8px] text-black resize-none'
-          />
-        </div>
+      <div className='flex flex-col gap-2'>
         <div className='flex justify-between'>
-          <label className='flex justify-start items-center text-[14px] w-14 mr-4 text-gray-700'>버스</label>
+          <label className='flex justify-start pt-2 text-[14px] w-14 mr-4 text-gray-700'>버스</label>
           <textarea
             placeholder='오시는 길 내용을 입력하세요.'
             {...register('navigationDetail.bus')}
+            className='h-[64px] w-full pl-[8px] py-[9px] border text-[12px] rounded-[8px] text-black resize-none'
+          />
+        </div>
+        <div className='flex justify-between w-full gap-4'>
+          <label className='flex justify-start pt-2 text-[14px] w-14 text-gray-700'>지하철</label>
+          <textarea
+            placeholder='오시는 길 내용을 입력하세요.'
+            {...register('navigationDetail.subway')}
             className='h-[64px] w-full pl-[8px] py-[9px] border text-[12px] rounded-[8px] text-black resize-none'
           />
         </div>
