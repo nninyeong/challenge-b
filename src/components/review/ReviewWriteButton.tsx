aaaf9379afa9769c91deaf5c62baea0abc426848
GiSeong Lee <cj8928@gmail.com>
2024-11-03 04:25:54 +0900

    add: 후기 작성 버튼 클릭시 로그인 여부, 청첩장 제작 여부 확인 함수 생성

--- src/components/review/ReviewWriteButton.tsx
+++ src/components/review/ReviewWriteButton.tsx
@@ -1,16 +1,53 @@
 'use client';
 
+import { checkMadeInvitations } from '@/utils/checkMadeInvitations';
+import browserClient from '@/utils/supabase/client';
+import { useRouter } from 'next/navigation';
+import { Notify } from 'notiflix';
+
 const ReviewWriteButton = ({
   setOpenBottomSheet,
 }: {
   setOpenBottomSheet: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
+  const router = useRouter();
+
+  const getUserId = async () => {
+    const { data } = await browserClient.auth.getUser();
+    return data.user?.id;
+  };
+  const checkAccessPermissions = async () => {
+    const userId = await getUserId();
+
+    if (!userId) {
+      Notify.failure('먼저 로그인을 해주세요.');
+      router.push('/signin');
+      return false;
+    }
+
+    const isMadeInvitation = await checkMadeInvitations(userId);
+    if (!isMadeInvitation) {
+      Notify.failure('먼저 청첩장을 만들어주세요.');
+      router.push('/create/card');
+      return false;
+    }
+
+    return true;
+  };
+
+  const handleOpenBottomSheetButton = async () => {
+    const hasPermissions = await checkAccessPermissions();
+    if (hasPermissions) {
+      setOpenBottomSheet(true);
+    }
+  };
+
   return (
     <button
       className='fixed bottom-[13px] left-1/2 rounded-[300px] bg-primary300 
     text-white flex justify-center items-center w-[142px] h-[40px] -translate-x-1/2
     '
-      onClick={() => setOpenBottomSheet(true)}
+      onClick={handleOpenBottomSheetButton}
     >
       <img
         src='/assets/images/icons/white-edit-contained-selceted.svg'
