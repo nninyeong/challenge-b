1e6fd152b9bbf5fbe5a35cd1271edf1d63b7a0a7
Subin, Han <ella0411@naver.com>
2024-11-03 18:13:35 +0900

    feat: 로그아웃 시 모든 쿼리 캐시 삭제

--- src/hooks/auth/useSignOut.ts
+++ src/hooks/auth/useSignOut.ts
@@ -1,12 +1,15 @@
 import { createClient } from '@/utils/supabase/client';
+import { useQueryClient } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
 
 const useSignOut = () => {
   const client = createClient();
   const router = useRouter();
+  const queryClient = useQueryClient();
 
   const handleSignOut = async () => {
     await client.auth.signOut();
+    queryClient.clear();
     router.refresh();
   };
 
