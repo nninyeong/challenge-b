cede4953ea46f55febdc4e5a91ac7ddb36b4a083
sHyunis <162306191+sHyunis@users.noreply.github.com>
2024-11-04 03:01:57 +0900

    feat:갤러리이미지삭제기능추가

--- src/components/create/preview/GalleryPreview.tsx
+++ src/components/create/preview/GalleryPreview.tsx
@@ -1,14 +1,9 @@
 import { InvitationFormType } from '@/types/invitationFormType.type';
 import { Control } from 'react-hook-form';
 import WeddingGallery from '@/components/card/WeddingGallery';
-import { DndProvider } from 'react-dnd';
-import { HTML5toTouch } from '@/lib/reactDnd/dndBackends';
+
 const GalleryPreview = ({ control }: { control: Control<InvitationFormType> }) => {
-  return (
-    <DndProvider options={HTML5toTouch}>
-      <WeddingGallery control={control} />;
-    </DndProvider>
-  );
+  return <WeddingGallery control={control} />;
 };
 
 export default GalleryPreview;
