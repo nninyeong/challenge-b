8e85bcadf380a7be59711c1af0ef3647804c4878
Subin, Han <ella0411@naver.com>
2024-11-04 13:18:02 +0900

    fix: 리뷰페이지 바로가기 삭제

--- src/components/main/ReviewCarousel.tsx
+++ src/components/main/ReviewCarousel.tsx
@@ -4,17 +4,9 @@ import Carousel from './Carousel';
 const ReviewCarousel = () => {
   return (
     <div className='flex flex-col gap-[40px]'>
-      <div className='flex  justify-between items-center'>
-        <div>
-          <h1 className='text-[20px] font-semibold'>- 서비스명 이용 후기</h1>
-          <p className='text-[14px]'>To Speed up Your Creative Woreflow</p>
-        </div>
-        <Link
-          href={'/review'}
-          className='text-[16px] h-[32px] w-[80px] bg-gray-400 flex justify-center items-center rounded-full'
-        >
-          바로가기
-        </Link>
+      <div>
+        <h1 className='text-[20px] font-semibold'>- 서비스명 이용 후기</h1>
+        <p className='text-[14px]'>To Speed up Your Creative Woreflow</p>
       </div>
       <Carousel />
     </div>
