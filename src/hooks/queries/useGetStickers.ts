552c4d9f33fc084b03e054a9375effcf1a8b6b0a
minyeong <minyk0512@gmail.com>
2024-11-04 18:35:29 +0900

    fix: supabase에서 받아온 파일에 예외 발생시키는 파일 있는지 검사

--- src/hooks/queries/useGetStickers.ts
+++ src/hooks/queries/useGetStickers.ts
@@ -4,51 +4,56 @@ import { useQuery } from '@tanstack/react-query';
 import { QUERY_KEYS } from '@/hooks/queries/queryKeys';
 import { StickerImage } from '@/types/stickerData.types';
 
-const client = createClient();
-
 const fetchAllStickerImages = async (): Promise<Record<string, StickerImage[]>> => {
-  const { data, error } = await client.storage.from('stickers').list('', { limit: 1000 });
-  if (error) {
+  try {
+    const { data, error } = await client.storage.from('stickers').list('', { limit: 1000 });
+    if (error) {
+      console.error(error);
+    }
+
+    const validFiles = data?.filter((file) => file.name !== '.emptyFolderPlaceholder') || [];
+    const stickersByCategory: Record<string, StickerImage[]> = {};
+
+    const getImageDimensions = (url: string): Promise<{ width: number; height: number }> => {
+      return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.src = url;
+        img.onload = () => resolve({ width: img.width, height: img.height });
+        img.onerror = reject;
+      });
+    };
+
+    await Promise.all(
+      validFiles?.map(async (file) => {
+        const [category] = file.name.split('-');
+        const { data: publicUrlData } = client.storage.from('stickers').getPublicUrl(file.name);
+
+        const dimensions = await getImageDimensions(publicUrlData.publicUrl);
+
+        const stickerImage: StickerImage = {
+          id: file.name,
+          url: publicUrlData.publicUrl,
+          category,
+          width: dimensions.width,
+          height: dimensions.height,
+        };
+
+        if (!stickersByCategory[category]) {
+          stickersByCategory[category] = [];
+        }
+
+        stickersByCategory[category].push(stickerImage);
+      }) || [],
+    );
+
+    return stickersByCategory;
+  } catch (error) {
     console.error(error);
-    throw new Error('스티커 fetch 에러: ', error);
+    throw error;
   }
-
-  const stickersByCategory: Record<string, StickerImage[]> = {};
-
-  const getImageDimensions = (url: string): Promise<{ width: number; height: number }> => {
-    return new Promise((resolve, reject) => {
-      const img = new Image();
-      img.src = url;
-      img.onload = () => resolve({ width: img.width, height: img.height });
-      img.onerror = reject;
-    });
-  };
-
-  await Promise.all(
-    data?.map(async (file) => {
-      const [category] = file.name.split('-');
-      const { data: publicUrlData } = client.storage.from('stickers').getPublicUrl(file.name);
-
-      const dimensions = await getImageDimensions(publicUrlData.publicUrl);
-
-      const stickerImage: StickerImage = {
-        id: file.name,
-        url: publicUrlData.publicUrl,
-        category,
-        width: dimensions.width,
-        height: dimensions.height,
-      };
-
-      if (!stickersByCategory[category]) {
-        stickersByCategory[category] = [];
-      }
-
-      stickersByCategory[category].push(stickerImage);
-    }) || [],
-  );
-
-  return stickersByCategory;
 };
+
+const client = createClient();
 
 export const useGetAllStickers = () => {
   return useQuery({
