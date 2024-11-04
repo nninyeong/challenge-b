05661811b229c10baae261f6916e94e2f92295d9
minyeong <minyk0512@gmail.com>
2024-11-04 14:58:43 +0900

    style: width 설정

--- src/components/ui/FixedQuickLink.tsx
+++ src/components/ui/FixedQuickLink.tsx
@@ -2,8 +2,8 @@ import LinkToCreateCard from '@/components/ui/LinkToCreateCard';
 
 const FixedQuickLink = () => {
   return (
-    <div className='border border-gray-200 fixed left-0 right-0 bottom-[16px] x-[343px] h-[67px] bg-white z-50 rounded-[12px] flex justify-between items-center mx-auto px-[16px] py-[12px]'>
-      <div className='flex-1'>
+    <div className='border border-gray-200 fixed bottom-[16px] x-[343px] h-[67px] bg-white z-50 rounded-[12px] flex justify-between items-center mx-auto px-[16px] py-[12px]'>
+      <div className='w-[163px]'>
         <h3 className='text-[16px] text-gray-900 font-bold'>나만의 청첩장 만들기</h3>
         <p className='text-[12px] text-gray-700 font-medium'>바로 제작해보세요!</p>
       </div>
