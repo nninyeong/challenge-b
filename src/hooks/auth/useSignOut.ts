e80af3c19a232217f830e1ca9873419c1f01c7db
Subin, Han <ella0411@naver.com>
2024-11-04 10:45:56 +0900

    fix: 전체 데이터 -> 유저 관련 데이터 변경

--- src/hooks/auth/useSignOut.ts
+++ src/hooks/auth/useSignOut.ts
@@ -1,15 +1,22 @@
 import { createClient } from '@/utils/supabase/client';
 import { useQueryClient } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
+import { QUERY_KEYS } from '../queries/queryKeys';
+import useInvitationIdByPathname from '../invitation/useInvitationIdByPathname';
 
 const useSignOut = () => {
   const client = createClient();
   const router = useRouter();
   const queryClient = useQueryClient();
+  const { invitationId } = useInvitationIdByPathname();
 
   const handleSignOut = async () => {
     await client.auth.signOut();
-    queryClient.clear();
+    queryClient.invalidateQueries({ queryKey: QUERY_KEYS.guestBook(invitationId) });
+    queryClient.invalidateQueries({ queryKey: QUERY_KEYS.invitation() });
+    queryClient.invalidateQueries({ queryKey: QUERY_KEYS.authUsers() });
+    queryClient.invalidateQueries({ queryKey: QUERY_KEYS.invitationCard() });
+
     router.refresh();
   };
 
