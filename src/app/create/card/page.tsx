c3844fd435d550d032a5fc61a637a9087fca5846
GiSeong Lee <cj8928@gmail.com>
2024-11-04 20:01:59 +0900

    feat: 폼 크기 매퍼 적용

--- src/app/create/card/page.tsx
+++ src/app/create/card/page.tsx
@@ -23,6 +23,7 @@ import Button from '@/components/ui/Button';
 import { revalidateInvitation } from '@/utils/revalidateInvitation';
 import { Notify } from 'notiflix';
 import EventBus from '@/utils/EventBus';
+import createCardFormHeightMapper from '@/utils/createCardFormHeightMapper';
 
 const DELAY_TIME: number = 300;
 
@@ -215,7 +216,7 @@ const CreateCardPage = () => {
             </div>
             <div className='fixed bottom-0 left-0 right-0 px-4 z-10'>
               <form
-                className={`flex flex-col bg-white shadow-xl px-4 py-4 object-cover rounded-lg ${toggleInput ? 'h-[320px]' : 'h-[54px]'} mb-[8px] z-10`}
+                className={`flex flex-col bg-white shadow-xl px-4 py-4 object-cover rounded-lg ${createCardFormHeightMapper(toggleInput, orderList[currentStep].name[nameIndex])} mb-[8px] z-10`}
                 onSubmit={methods.handleSubmit(onSubmit)}
               >
                 <div className='flex justify-between items-center'>
