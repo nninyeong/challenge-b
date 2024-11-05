cf21b1d516356a9f2751859478a08a063913e97f
lukby2457 <kjh7442455@gmail.com>
2024-11-05 18:46:15 +0900

    style: 디자인 색상 gray로 변경

--- src/components/guestbook/GuestBookPagination.tsx
+++ src/components/guestbook/GuestBookPagination.tsx
@@ -10,14 +10,14 @@ const GuestBookPagination = ({
   totalPages: number;
 }) => {
   return (
-    <div className='h-10 px-1 bg-primary300 flex justify-between items-center rounded-full'>
+    <div className='h-10 px-1 bg-gray-600 flex justify-between items-center rounded-full'>
       {Array.from({ length: 9 }, (_, index) => (
         <button
           key={index + 1}
           onClick={() => setPage(index + 1)}
           disabled={index + 1 > totalPages}
           className={`w-8 h-8 px-2 rounded-full text-[16px] 
-          ${page === index + 1 ? 'bg-white text-primary300 text-[20px]' : 'text-white'}
+          ${page === index + 1 ? 'bg-white text-gray-900 text-[20px]' : 'text-white'}
           ${index + 1 > totalPages ? 'opacity-50 cursor-not-allowed' : ''}`}
         >
           {index + 1}
