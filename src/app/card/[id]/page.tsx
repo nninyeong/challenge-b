6725a7b21c218280fdf606aa807e9427e89b6684
minyeong <minyk0512@gmail.com>
2024-11-06 15:35:13 +0900

    feat: 배포된 청첩장 페이지에 접속하는 경우 userId와 isPrivate을 확인해 보여지는지 결정

--- src/app/card/[id]/page.tsx
+++ src/app/card/[id]/page.tsx
@@ -1,8 +1,7 @@
 import { notFound } from 'next/navigation';
 import { supabase } from '@/utils/supabase/createClient';
 import { convertToCamelCase } from '@/utils/convert/invitaitonTypeConvert';
-import { convertOrderToComponent } from '@/utils/convert/convertOrderToComponent';
-import { Fragment } from 'react';
+import SharedCard from '@/components/card/SharedCard';
 
 export const generateStaticParams = async () => {
   const { data } = await supabase.from('invitation').select('id');
@@ -23,24 +22,9 @@ const fetchInvitationData = async (id: string) => {
 
 const CardPage = async ({ params }: { params: { id: string } }) => {
   const invitation = await fetchInvitationData(params.id);
-  const { isPrivate, renderOrder, ...invitationData } = convertToCamelCase(invitation);
-  const fontStyle = invitation.main_photo_info.fontName;
-  const bgColor = invitation.bg_color;
+  const invitationData = convertToCamelCase(invitation);
 
-  return isPrivate ? (
-    <div>아직 공개되지 않은 청첩장입니다.</div>
-  ) : (
-    <div
-      className={`flex flex-col gap-[56px] font-${fontStyle}`}
-      style={{ backgroundColor: `rgba(${bgColor.r}, ${bgColor.g}, ${bgColor.b}, ${bgColor.a})` }}
-    >
-      {renderOrder
-        .sort((a, b) => a.order - b.order)
-        .map(({ typeOnSharedCard }, index) => (
-          <Fragment key={index}>{convertOrderToComponent(typeOnSharedCard, invitationData)}</Fragment>
-        ))}
-    </div>
-  );
+  return <SharedCard invitationFormData={invitationData} />;
 };
 
 export default CardPage;
