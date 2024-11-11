c529112e5c012d8db17b38207d41a3dfd0b61135
minyeong <minyk0512@gmail.com>
2024-11-12 02:47:36 +0900

    add: sentry 세팅 추가

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,27 +1,32 @@
 'use client';
 
+import * as Sentry from '@sentry/nextjs';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
+import Button from '@/components/ui/Button';
 
 const GlobalError = ({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) => {
   const { refresh } = useRouter();
+
   useEffect(() => {
+    Sentry.captureException(error);
     console.error(error);
   }, [error]);
 
   return (
     <div>
       <h2>{`:(`}</h2>
-      <h3>에러 메세지: {error.message}</h3>
-      <button
+      <h3>에러 메시지: {error.message}</h3>
+      <Button
         onClick={() => {
           reset();
           refresh();
         }}
       >
         다시 시도
-      </button>
+      </Button>
     </div>
   );
 };
+
 export default GlobalError;
