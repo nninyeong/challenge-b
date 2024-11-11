9395de0e615838a17d9b9f0e75cac3dbcc1d24fa
sHyunis <162306191+sHyunis@users.noreply.github.com>
2024-11-11 22:12:27 +0900

    fix:error

--- src/components/create/CreateGuestInfo.tsx
+++ src/components/create/CreateGuestInfo.tsx
@@ -2,24 +2,15 @@
 import { InvitationFormType } from '@/types/invitationFormType.type';
 import GuestBook from '@/components/guestbook/GuestBook';
 import EventStatus from '@/components/create/EventStatus';
-import { useFontStore, useFontColorStore } from '@/store/useFontStore';
 type GuestInfoPropType = Pick<
   InvitationFormType,
   'attendance' | 'guestbook' | 'dDay' | 'weddingInfo' | 'mainPhotoInfo'
 >;
 
 const GuestInfo = ({ attendance, guestbook, dDay, weddingInfo, mainPhotoInfo }: GuestInfoPropType) => {
-  const fontSize = useFontStore((state) => state.fontSize);
-  const fontColor = useFontColorStore((state) => state.fontColor);
-  const rgbaColor = `rgba(${fontColor.r}, ${fontColor.g}, ${fontColor.b}, ${fontColor.a})`;
   return (
     <>
-      {guestbook && (
-        <GuestBook
-          fontSize={fontSize}
-          fontColor={rgbaColor}
-        />
-      )}
+      {guestbook && <GuestBook />}
       {attendance && dDay && (
         <div className='ml-4 border border-gray-200 left-1/2 bottom-[16px] w-[343px] h-[67px] bg-white rounded-[12px] flex justify-between items-center px-[16px] py-[12px]'>
           <EventStatus
--- src/components/guestbook/GuestBookCard.tsx
+++ src/components/guestbook/GuestBookCard.tsx
@@ -4,7 +4,7 @@ import { GuestBookEntry } from '@/types/guestBookEntry.types';
 import DeleteGuestBookAccordion from '@/components/guestbook/DeleteGuestBookAccordion';
 import useGuestBookDeleteButton from '@/hooks/guestbook/useGuestBookDeleteButton';
 import convertToUserTimezone from '@/utils/date/dayToKst';
-import { useFontStore } from '@/store/useFontStore';
+
 const GuestBookCard = ({
   guestBook,
   invitationId,
@@ -16,21 +16,14 @@ const GuestBookCard = ({
 }) => {
   const { isAccordionOpen, toggleAccordion } = useGuestBookDeleteButton();
   const { year, month, day, hour, minute } = convertToUserTimezone(guestBook.created_at);
-  
 
   return (
-    <div
-      
-      className='text-black w-full px-4 mb-4'
-    >
+    <div className='text-black w-full px-4 mb-4'>
       <div className='bg-gray-50 rounded-xl px-4'>
         <div className='flex justify-between pt-4'>
           <div className='text-gray-900 font-bold'>{guestBook.name}</div>
           <div className='flex items-center gap-1'>
-            <div
-             
-              className='text-[12px] text-gray-300'
-            >{`${year}-${month}-${day} ${hour}:${minute}`}</div>
+            <div className='text-[12px] text-gray-300'>{`${year}-${month}-${day} ${hour}:${minute}`}</div>
             <img
               src='/assets/images/icons/x-03-gray.svg'
               alt='x'
