151e1abff46226c714e06a5ce4a3e8ffc19de85e
minyeong <minyk0512@gmail.com>
2024-11-11 18:07:06 +0900

    feat: 메타데이터 오픈그래프 이미지 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,14 @@ const SUIT = localFont({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL(process.env.NEXT_PUBLIC_DOMAIN as string),
+  openGraph: {
+    images: [
+      {
+        url: '/opengraph-image',
+      },
+    ],
+  },
   title: '드림카드 | 나만의 모바일 청첩장',
   description: '모바일 청첩장을 직접 제작하고 커스텀할 수 있는 서비스입니다. 소중한 날을 더 특별하게 만들어보세요!',
 };
---
+++ src/app/opengraph-image.tsx
@@ -0,0 +1,54 @@
+import { ImageResponse } from 'next/og';
+
+export const runtime = 'edge';
+
+export const alt = '드림카드';
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = 'image/png';
+function arrayBufferToBase64(buffer: ArrayBuffer) {
+  let binary = '';
+  const bytes = new Uint8Array(buffer);
+  const len = bytes.byteLength;
+  for (let i = 0; i < len; i++) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+  return btoa(binary);
+}
+
+export default async function Image() {
+  const logoBuffer = await fetch(new URL('../../public/assets/images/branding/2D-logo.png', import.meta.url)).then(
+    (res) => res.arrayBuffer(),
+  );
+
+  const logoSrc = `data:image/png;base64,${arrayBufferToBase64(logoBuffer)}`;
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 128,
+          background: '#FF6666',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <img
+          src={logoSrc}
+          alt=''
+          width='300'
+        />
+      </div>
+    ),
+    {
+      ...size,
+    },
+  );
+}
