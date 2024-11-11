a9295ca87dcef74d2a652d8fc6e16be2656f9cf9
lukby2457 <kjh7442455@gmail.com>
2024-11-11 18:04:21 +0900

    fix: 카카오페이 모달 이미지 제외 디자인 반영 / guestbook, navigation 폴더 구조 변경

--- src/components/create/modal/KakaoPayModal.tsx
+++ src/components/create/modal/KakaoPayModal.tsx
@@ -4,15 +4,18 @@ type ModalProps = {
   onClose: () => void;
   onSave: () => void;
   onChange: (value: string) => void;
-}
+};
 
 const KakaoPayModal: React.FC<ModalProps> = ({ isModalOpen, value, onClose, onSave, onChange }) => {
   if (!isModalOpen) return null;
 
   return (
-    <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50'>
-      <div className='bg-white p-4 rounded-md'>
-        <h2 className='text-lg font-bold mb-4'>카카오페이 정보 입력</h2>
+    <div
+      className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50'
+      onClick={onClose}
+    >
+      <div className='bg-white p-4 rounded-md w-[343px]' onClick={(e) => e.stopPropagation()}>
+        <h2 className='text-[16px] font-bold mb-4'>카카오페이 등록</h2>
         <input
           type='text'
           value={value}
@@ -20,14 +23,12 @@ const KakaoPayModal: React.FC<ModalProps> = ({ isModalOpen, value, onClose, onSa
           placeholder='카카오페이 송금 링크 복사'
           className='w-full px-4 py-2 mb-4 border rounded'
         />
-        <div className='flex justify-end gap-2'>
-          <button onClick={onClose} className='px-4 py-2 bg-gray-300 rounded'>
-            취소
-          </button>
-          <button onClick={onSave} className='px-4 py-2 bg-blue-500 text-white rounded'>
-            저장
-          </button>
-        </div>
+        <button
+          onClick={onSave}
+          className='w-full h-12 bg-primary300 text-[16px] font-bold text-white rounded'
+        >
+          저장
+        </button>
       </div>
     </div>
   );
