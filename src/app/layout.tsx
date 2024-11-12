d03194aeab0b83d59587e79b24dde49e3dbc5d1a
sHyunis <162306191+sHyunis@users.noreply.github.com>
2024-11-12 15:17:07 +0900

    구글 엔진 head추가 , 애널리틱스추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,11 @@ import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
 import Providers from '@/app/provider';
-import Script from 'next/script';
 import { ReviewBottomSheetProvider } from '@/provider/reviewBottomSheetProvider';
 import DndProviderWrapper from '@/components/layouts/DndProvider';
 import { MOBILE_VIEW_WIDTH } from '@/constants/screenSize';
 import InitClientSettings from '@/utils/settings/InitClientSettings';
+import Script from 'next/script';
 
 const SUIT = localFont({
   src: '../../public/assets/fonts/SUIT-Variable.ttf',
@@ -38,18 +38,38 @@ export default function RootLayout({
       className='overflow-x-hidden'
     >
       <head>
+        {/* GA4 스크립트 로드 */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+          strategy='afterInteractive'
+        />
+        {/* GA4 초기화 및 페이지 추적 설정 */}
+        <Script
+          id='ga-init'
+          strategy='afterInteractive'
+        >
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
         <meta
           name='viewport'
           content='width=device-width, initial-scale=1.0, maximun-scale=1.0, user-scalable=no'
+        />
+        <meta
+          name='google-site-verification'
+          content='fZ2v9PdapWDy5-avIG7z7jgsuGEu3iUiwxxgqyGZHEc'
         />
       </head>
       <body
         className={`${SUIT.variable} font-main antialiased flex flex-col min-h-[calc(var(--vh)_*_100)] max-w-[${MOBILE_VIEW_WIDTH}] mx-auto bg-gray-100`}
       >
-        <Script
-          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_KAKAOAPP_KEY}&libraries=services&autoload=false`}
-          strategy='beforeInteractive'
-        />
         <Providers>
           <ReviewBottomSheetProvider isReviewBottomSheetOpen={false}>
             <div className='w-full h-full flex-1 bg-white'>
