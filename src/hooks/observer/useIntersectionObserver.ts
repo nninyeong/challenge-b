16dccee736e9580378b5853cfd8c93c94ff6861b
GiSeong Lee <cj8928@gmail.com>
2024-11-12 21:37:21 +0900

    fix: 옵저버 디바운스 적용 및 감지 범위 수정

--- src/hooks/observer/useIntersectionObserver.ts
+++ src/hooks/observer/useIntersectionObserver.ts
@@ -1,25 +1,26 @@
 import { MutableRefObject, useRef } from 'react';
 import { StepType } from '../create/useFormStepController';
+import { debounce } from '@/utils/debounce';
 
 type ObserverOptions = {
   root?: HTMLElement | null;
   rootMargin?: string;
-  threshold?: number;
+  threshold?: number[];
 };
 
 const DEFAULT_OPTIONS: ObserverOptions = {
   root: null,
-  rootMargin: '0px 0px -70% 0px',
-  threshold: 0.2,
+  rootMargin: '-30% 0px',
 };
-
+const DELAY_TIME: number = 300;
 export const useIntersectionObserver = (
   refs: MutableRefObject<{ [key: string]: { ref: HTMLDivElement | null; order: number; inputOrder: number } }>,
   setCurrentStep: (step: StepType) => void,
 ) => {
   const observers = useRef<IntersectionObserver[]>([]);
   const isNavigating = useRef<boolean>(false);
-  const observerCallback = (entries: IntersectionObserverEntry[]) => {
+
+  const observerCallback = debounce((entries: IntersectionObserverEntry[]) => {
     if (isNavigating.current) return;
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
@@ -32,11 +33,14 @@ export const useIntersectionObserver = (
         });
       }
     });
-  };
+  }, DELAY_TIME);
+
   const initializeObserver = () => {
     Object.values(refs.current).forEach((ref, index) => {
       if (ref) {
-        const observer = new IntersectionObserver(observerCallback, DEFAULT_OPTIONS);
+        const elementHeight = ref.ref!.clientHeight;
+        const threshold = elementHeight! > window.innerHeight ? 0.1 : 0.6;
+        const observer = new IntersectionObserver(observerCallback, { ...DEFAULT_OPTIONS, threshold: threshold });
         observer.observe(ref.ref as Element);
         observers.current[index] = observer;
       }
