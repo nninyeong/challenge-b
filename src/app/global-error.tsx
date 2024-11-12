c35976dcb82d11fd8159d88edba17ebe6a12e3c8
minyeong <minyk0512@gmail.com>
2024-11-12 10:52:44 +0900

    delete: sentry 설정 제거 This reverts commit c529112e5c012d8db17b38207d41a3dfd0b61135.

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,32 +1,27 @@
 'use client';
 
-import * as Sentry from '@sentry/nextjs';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
-import Button from '@/components/ui/Button';
 
 const GlobalError = ({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) => {
   const { refresh } = useRouter();
-
   useEffect(() => {
-    Sentry.captureException(error);
     console.error(error);
   }, [error]);
 
   return (
     <div>
       <h2>{`:(`}</h2>
-      <h3>에러 메시지: {error.message}</h3>
-      <Button
+      <h3>에러 메세지: {error.message}</h3>
+      <button
         onClick={() => {
           reset();
           refresh();
         }}
       >
         다시 시도
-      </Button>
+      </button>
     </div>
   );
 };
-
 export default GlobalError;
