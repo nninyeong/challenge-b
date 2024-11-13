b130f947d8da7879b265ce5915d7a4f6b9c39696
minyeong <minyk0512@gmail.com>
2024-11-14 03:19:38 +0900

    style: 데스크탑 시안 반영

--- src/components/main/BrandingCarousel/BrandingCarousel.tsx
+++ src/components/main/BrandingCarousel/BrandingCarousel.tsx
@@ -8,13 +8,13 @@ const BrandingCarousel = () => {
 
   const moveCarousel = useCallback(() => {
     setIsTransitioning(true);
-    setCurrentIndex((prev) => (prev += 1));
+    setCurrentIndex((prev) => prev + 1);
   }, []);
 
   useEffect(() => {
     const timerId = setInterval(() => {
       moveCarousel();
-    }, 2000);
+    }, 3000);
 
     return () => clearInterval(timerId);
   }, [currentIndex, moveCarousel]);
@@ -38,9 +38,9 @@ const BrandingCarousel = () => {
   }, [currentIndex]);
 
   return (
-    <div className='relative overflow-hidden w-full h-[384px] mb-[56px]'>
+    <div className='relative overflow-hidden w-full desktop:h-[588px] mobile:h-[384px] mb-[56px] bg-black'>
       <div
-        className={`flex ${isTransitioning && 'transition-transform duration-500'}`}
+        className={`desktop:hidden flex ${isTransitioning && 'transition-transform duration-500'}`}
         style={{ transform: `translateX(-${currentIndex * 100}%)` }}
       >
         <BrandingCarouselItem
@@ -64,12 +64,43 @@ const BrandingCarousel = () => {
           description={`소중한 기억을 위한 맞춤형\n청첩장, 드림카드와 함께`}
         />
       </div>
-      <div className='absolute bottom-4 left-1/2 transform -translate-x-1/2 w-[294px] h-[2px] bg-gray-400 z-50'>
+      <div
+        className={`hidden desktop:flex ${isTransitioning && 'transition-transform duration-500'}`}
+        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+      >
+        <BrandingCarouselItem
+          src='/assets/images/branding/desktop-main-mockup-3.webp'
+          description={`드림카드로 특별한 초대,\n참석 여부도 손쉽게`}
+        />
+        <BrandingCarouselItem
+          src='/assets/images/branding/desktop-main-mockup-1.webp'
+          description={`소중한 기억을 위한\n 맞춤형 청첩장, 드림카드와 함께`}
+        />
+        <BrandingCarouselItem
+          src='/assets/images/branding/desktop-main-mockup-2.webp'
+          description={`드림카드로 완성하는\n당신만의 특별한 무드`}
+        />
+        <BrandingCarouselItem
+          src='/assets/images/branding/desktop-main-mockup-3.webp'
+          description={`드림카드로 특별한 초대,\n참석 여부도 손쉽게`}
+        />
+        <BrandingCarouselItem
+          src='/assets/images/branding/desktop-main-mockup-1.webp'
+          description={`소중한 기억을 위한\n 맞춤형 청첩장, 드림카드와 함께`}
+        />
+      </div>
+      <div className='absolute bottom-4 desktop:bottom-[40px] left-1/2 transform -translate-x-1/2 w-[294px] desktop:w-[1136px] h-[2px] desktop:h-[4px] bg-gray-400 z-50'>
         <div
           style={{
-            transform: `translateX(${(currentIndex % MOCKUP_NUMBER) * 98}px)`,
+            transform: `translateX(${((currentIndex - 1) % MOCKUP_NUMBER) * 98}px)`,
           }}
-          className='w-[98px] h-full bg-white'
+          className='desktop:hidden w-[98px] h-full bg-white'
+        ></div>
+        <div
+          style={{
+            transform: `translateX(${((currentIndex - 1) % MOCKUP_NUMBER) * 378}px)`,
+          }}
+          className='hidden desktop:block w-[378px] h-full bg-white'
         ></div>
       </div>
     </div>
--- src/components/main/BrandingCarousel/BrandingCarouselItem.tsx
+++ src/components/main/BrandingCarousel/BrandingCarouselItem.tsx
@@ -1,16 +1,16 @@
 const BrandingCarouselItem = ({ src, description }: { src: string; description: string }) => {
   return (
-    <div className='relative w-full h-full flex-shrink-0'>
+    <div className='relative w-full desktop:h-[588px] h-full flex-shrink-0'>
       <img
-        className='w-full h-full'
+        className='w-full h-full object-cover object-top'
         src={src}
         alt={description}
       />
       <div
-        className='absolute bottom-0 w-full h-[134px]'
+        className='absolute bottom-0 w-full desktop:h-[278px] mobile:h-[134px]'
         style={{ background: 'linear-gradient(181deg, rgba(0, 0, 0, 0.00) 7.02%, rgba(0, 0, 0, 0.90) 99.4%)' }}
       ></div>
-      <p className='absolute bottom-[32px] mx-[40px] text-[28px] leading-[33.6px] text-white font-semibold whitespace-pre-line'>
+      <p className='absolute bottom-[32px] desktop:bottom-[80px] ml-[40px] desktop:ml-[152px] text-[28px] desktop:text-[48px] leading-[120%] text-white font-semibold whitespace-pre-line'>
         {description}
       </p>
     </div>
