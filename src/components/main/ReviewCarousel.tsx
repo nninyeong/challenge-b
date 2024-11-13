6f35e731a0466d579274aac7c9fface5063f4734
Subin, Han <ella0411@naver.com>
2024-11-13 16:48:02 +0900

    style: 데스크탑 버전 추가

--- src/components/main/ReviewCarousel.tsx
+++ src/components/main/ReviewCarousel.tsx
@@ -2,10 +2,10 @@ import Carousel from './Carousel';
 
 const ReviewCarousel = () => {
   return (
-    <div className='flex flex-col gap-[24px]'>
+    <div className='flex flex-col desktop:gap-[56px] mobile:gap-[24px]'>
       <div>
-        <h1 className='text-[20px] font-semibold'>드림카드 이용 후기</h1>
-        <p className='text-[14px]'>To Speed up Your Creative Woreflow</p>
+        <h1 className='desktop:text-[36px] mobile:text-[20px] font-semibold'>드림카드 이용 후기</h1>
+        <p className='desktop:text-[24px] mobile:text-[14px]'>To Speed up Your Creative Woreflow</p>
       </div>
       <Carousel />
     </div>
