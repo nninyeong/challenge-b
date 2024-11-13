7fcaa65db1f00936a25a9e4f19948947327c3286
minyeong <minyk0512@gmail.com>
2024-11-14 00:37:10 +0900

    feat: 모바일 캐러셀 적용

--- src/app/(defaultLayout)/page.tsx
+++ src/app/(defaultLayout)/page.tsx
@@ -6,7 +6,7 @@ import FixedQuickLink from '@/components/ui/FixedQuickLink';
 import Gallery from '@/components/main/Gallery';
 import MoodKeyword from '@/components/main/MoodKeyword';
 import { handleAuthCallback } from '@/utils/auth/authCallbackHandler';
-import BrandingCarouselItem from '@/components/main/BrandingCarousel/BrandingCarouselItem';
+import BrandingCarousel from '@/components/main/BrandingCarousel/BrandingCarousel';
 
 export default function Home() {
   const router = useRouter();
@@ -18,20 +18,7 @@ export default function Home() {
   return (
     <div className='items-center h-full'>
       <FixedQuickLink />
-      <div className='flex overflow-hidden w-full h-[384px] mb-[56px]'>
-        <BrandingCarouselItem
-          src='/assets/images/branding/main-mockup-1.webp'
-          description={`소중한 기억을 위한 맞춤형\n청첩장, 드림카드와 함께`}
-        />
-        <BrandingCarouselItem
-          src='/assets/images/branding/main-mockup-2.webp'
-          description={`드림카드로 완성하\n당신만의 특별한 무드`}
-        />
-        <BrandingCarouselItem
-          src='/assets/images/branding/main-mockup-3.webp'
-          description={`드림카드로 특별한\n초대, 참석 여부도 손쉽게`}
-        />
-      </div>
+      <BrandingCarousel />
       <div className='px-[16px] mb-[96px]'>
         <Gallery />
         <MoodKeyword />
---
+++ src/components/main/BrandingCarousel/BrandingCarousel.tsx
@@ -0,0 +1,71 @@
+import { useCallback, useEffect, useState } from 'react';
+import BrandingCarouselItem from '@/components/main/BrandingCarousel/BrandingCarouselItem';
+
+const MOCKUP_NUMBER = 3;
+const BrandingCarousel = () => {
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const [isTransitioning, setIsTransitioning] = useState(true);
+
+  const moveCarousel = useCallback(() => {
+    setIsTransitioning(true);
+    setCurrentIndex((prev) => (prev += 1));
+  }, []);
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      moveCarousel();
+    }, 2000);
+
+    return () => clearInterval(timerId);
+  }, [currentIndex, moveCarousel]);
+
+  useEffect(() => {
+    let timerId: NodeJS.Timeout;
+
+    if (currentIndex === 0) {
+      timerId = setTimeout(() => {
+        setCurrentIndex(MOCKUP_NUMBER);
+        setIsTransitioning(false);
+      }, 500);
+    } else if (currentIndex === MOCKUP_NUMBER + 1) {
+      timerId = setTimeout(() => {
+        setCurrentIndex(1);
+        setIsTransitioning(false);
+      }, 500);
+    }
+
+    return () => clearTimeout(timerId);
+  }, [currentIndex]);
+
+  return (
+    <div className='relative overflow-hidden w-full h-[384px] mb-[56px]'>
+      <div
+        className={`flex ${isTransitioning && 'transition-transform duration-500'}`}
+        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+      >
+        <BrandingCarouselItem
+          src='/assets/images/branding/main-mockup-3.webp'
+          description={`드림카드로 특별한\n초대, 참석 여부도 손쉽게`}
+        />
+        <BrandingCarouselItem
+          src='/assets/images/branding/main-mockup-1.webp'
+          description={`소중한 기억을 위한 맞춤형\n청첩장, 드림카드와 함께`}
+        />
+        <BrandingCarouselItem
+          src='/assets/images/branding/main-mockup-2.webp'
+          description={`드림카드로 완성하\n당신만의 특별한 무드`}
+        />
+        <BrandingCarouselItem
+          src='/assets/images/branding/main-mockup-3.webp'
+          description={`드림카드로 특별한\n초대, 참석 여부도 손쉽게`}
+        />
+        <BrandingCarouselItem
+          src='/assets/images/branding/main-mockup-1.webp'
+          description={`소중한 기억을 위한 맞춤형\n청첩장, 드림카드와 함께`}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default BrandingCarousel;
