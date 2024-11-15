ee23c7b555afcc5048fcdbbe2f3a4b667fb93877
Subin, Han <ella0411@naver.com>
2024-11-15 17:19:21 +0900

    fix: 시안 문구 수정

--- src/components/main/ReviewCarousel.tsx
+++ src/components/main/ReviewCarousel.tsx
@@ -5,7 +5,7 @@ const ReviewCarousel = () => {
     <div className='flex flex-col desktop:gap-[56px] gap-[24px]'>
       <div>
         <h1 className='desktop:text-[36px] text-[20px] font-semibold'>드림카드 이용 후기</h1>
-        <p className='desktop:text-[24px] text-[14px]'>To Speed up Your Creative Woreflow</p>
+        <p className='desktop:text-[24px] text-[14px]'>드림카드와 함께한 소중한 순간의 기록</p>
       </div>
       <Carousel />
     </div>
