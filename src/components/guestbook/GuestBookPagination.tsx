119a69ac748ba02d5cece3ba1e678bc4b0fc0de0
lukby2457 <kjh7442455@gmail.com>
2024-11-16 02:51:04 +0900

    fix: 9페이지 미만 버튼 disabled 해제된 사항 적용

--- src/components/guestbook/GuestBookPagination.tsx
+++ src/components/guestbook/GuestBookPagination.tsx
@@ -15,7 +15,7 @@ const GuestBookPagination = ({
 
     if (totalPages <= maxButtons) {
       // If total pages is less than or equal to 9, show all pages
-      for (let i = 1; i <= totalPages; i++) {
+      for (let i = 1; i <= maxButtons; i++) {
         range.push(i);
       }
     } else {
@@ -38,15 +38,15 @@ const GuestBookPagination = ({
   const paginationRange = getPaginationRange();
 
   return (
-    <div className='h-10 px-1 bg-gray-600 flex justify-between items-center rounded-full'>
+    <div className="h-10 px-1 bg-gray-600 flex justify-between items-center rounded-full">
       {paginationRange.map((pageNumber, index) => (
         <button
           key={index}
           onClick={() => typeof pageNumber === 'number' && setPage(pageNumber)}
-          disabled={pageNumber === '...'}
+          disabled={pageNumber === '...' || (typeof pageNumber === 'number' && pageNumber > totalPages)}
           className={`w-8 h-8 rounded-full text-[16px] flex items-center justify-center
             ${page === pageNumber ? 'bg-white text-gray-900 text-[20px]' : 'text-white'}
-            ${pageNumber === '...' ? 'cursor-default' : ''}`}
+            ${pageNumber === '...' || (typeof pageNumber === 'number' && pageNumber > totalPages) ? 'cursor-default opacity-50' : ''}`}
         >
           {pageNumber}
         </button>
