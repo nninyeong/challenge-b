4a4f22a07064d4c0185b9cf377452bb26590eb4d
lukby2457 <kjh7442455@gmail.com>
2024-11-18 20:45:24 +0900

    fix: FAQ 카드 이미지 크기 조정

--- src/components/main/FaqCard.tsx
+++ src/components/main/FaqCard.tsx
@@ -2,7 +2,7 @@ const FaqCard = ({ answer }: { answer: string }) => {
   return (
     <div className='text-gray-800 text-[12px] desktop:text-[20px] font-medium bg-gray-50 w-full pt-4 pb-5 desktop:pt-6 desktop:pb-7'>
       <div className='flex gap-4 desktop:gap-12'>
-        <div className='w-[40px] h-[40px] relative flex-shrink-0'>
+        <div className='w-[40px] desktop:w-[60px] h-[40px] desktop:h-[60px] relative flex-shrink-0'>
           <img
             src='/assets/images/branding/3D-logo-with-text.png'
             alt='드림카드'
