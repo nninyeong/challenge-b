1dd4a612e31eb0ffd899e3b4227af050bf919091
GiSeong Lee <cj8928@gmail.com>
2024-11-18 23:41:20 +0900

    style: 메인 화면 꾸미기 마진 30px로 수정

--- src/components/create/MainViewInput.tsx
+++ src/components/create/MainViewInput.tsx
@@ -23,7 +23,7 @@ const MainViewInput = () => {
   };
 
   return (
-    <div className='mt-[38px] desktop:px-[87px]'>
+    <div className='mt-[30px] desktop:px-[87px]'>
       <div className='w-full flex justify-between items-center'>
         {DECORATE_IMAGE_TYPE.map((item) => (
           <div
