ac7006ac4bb6e29a75ade68ee644b0f7710fb459
Subin, Han <ella0411@naver.com>
2024-11-18 12:58:17 +0900

    Revert "style: 스타일 수정"
    This reverts commit 5a11c897e6f83bfc3c4c5819a45c0e909481082b.

--- src/components/main/ReviewCarousel.tsx
+++ src/components/main/ReviewCarousel.tsx
@@ -4,8 +4,8 @@ const ReviewCarousel = () => {
   return (
     <div className='flex flex-col desktop:gap-[56px] gap-[24px]'>
       <div>
-        <h1 className='desktop:text-[36px] text-[20px] font-semibold'>드림카드 이용 후기</h1>
-        <p className='desktop:text-[24px] text-[14px]'>드림카드와 함께한 소중한 순간의 기록</p>
+        <h1 className='desktop:text-[36px] text-[20px] font-bold'>드림카드 이용 후기</h1>
+        <p className='desktop:text-[24px] text-[14px] font-medium'>드림카드와 함께한 소중한 순간의 기록</p>
       </div>
       <Carousel />
     </div>
