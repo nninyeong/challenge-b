05c1a9f32da9852c4ae91b7842a0b28b0b8f8fc6
sHyunis <162306191+sHyunis@users.noreply.github.com>
2024-11-18 09:38:38 +0900

    fix:console

--- src/utils/deleteMyReview.ts
+++ src/utils/deleteMyReview.ts
@@ -1,10 +1,8 @@
 import browserClient from './supabase/client';
 
 const deleteMyReview = async (id: string) => {
-  console.log('삭제 요청 ID:', id); // 추가된 디버깅 코드
   const { data, error } = await browserClient.from('reviews').delete().eq('user_id', id);
   if (error) {
-    console.error('리뷰 삭제 오류:', error.message);
     throw new Error();
   }
   return data;
