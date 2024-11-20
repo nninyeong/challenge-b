abae8e654fd2d775ebb1184e51d311f318c5bf9f
minyeong <minyk0512@gmail.com>
2024-11-20 16:27:11 +0900

    test: date-fns 적용

--- src/utils/date/formatDate.ts
+++ src/utils/date/formatDate.ts
@@ -1,23 +1,18 @@
-import dayjs from 'dayjs';
 import { Notify } from 'notiflix';
-import localizedFormat from 'dayjs/plugin/localizedFormat';
-
-dayjs.extend(localizedFormat);
-dayjs.locale('ko');
+import { isValid, parse } from 'date-fns';
 
 export const formatDate = (value: string) => {
   const inputNumbers = value.replace(/\D/g, '');
 
   if (inputNumbers.length <= 4) return inputNumbers;
   else if (inputNumbers.length <= 6) return `${inputNumbers.slice(0, 4)}.${inputNumbers.slice(4)}`;
-  else {
-    const formattedDate = `${inputNumbers.slice(0, 4)}.${inputNumbers.slice(4, 6)}.${inputNumbers.slice(6, 8)}`;
-    const isValide = dayjs(formattedDate, 'YYYY.MM.DD').isValid();
-    if (!isValide) {
-      Notify.failure('유효한 날짜를 입력해주세요.');
-      return '';
-    }
 
-    return formattedDate;
+  const formattedDate = `${inputNumbers.slice(0, 4)}.${inputNumbers.slice(4, 6)}.${inputNumbers.slice(6, 8)}`;
+
+  const parsedDate = parse(formattedDate, 'yyyy.MM.dd', new Date());
+  if (!isValid(parsedDate)) {
+    Notify.failure('유효한 날짜를 입력해주세요.');
   }
+
+  return formattedDate;
 };
