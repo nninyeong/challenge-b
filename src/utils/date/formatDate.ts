dd9e06c56001620955d4e9f561cb7dd62a817b91
minyeong <minyk0512@gmail.com>
2024-11-20 19:39:59 +0900

    fix: 날짜를 date 객체로 변환해 사용

--- src/utils/date/formatDate.ts
+++ src/utils/date/formatDate.ts
@@ -1,5 +1,5 @@
 import { Notify } from 'notiflix';
-import { isValid, parse } from 'date-fns';
+import { isValid } from 'date-fns';
 
 export const formatDate = (value: string) => {
   const inputNumbers = value.replace(/\D/g, '');
@@ -7,12 +7,11 @@ export const formatDate = (value: string) => {
   if (inputNumbers.length <= 4) return inputNumbers;
   else if (inputNumbers.length <= 6) return `${inputNumbers.slice(0, 4)}.${inputNumbers.slice(4)}`;
 
-  const formattedDate = `${inputNumbers.slice(0, 4)}-${inputNumbers.slice(4, 6)}-${inputNumbers.slice(6, 8)}`;
-
-  const parsedDate = parse(formattedDate, 'yyyy-MM-dd', new Date());
-  if (!isValid(parsedDate)) {
+  const formattedDate = `${inputNumbers.slice(0, 4)}.${inputNumbers.slice(4, 6)}.${inputNumbers.slice(6, 8)}`;
+  const dateObj = new Date(formattedDate.replace(/\./g, '-'));
+  if (!isValid(dateObj)) {
     Notify.failure('유효한 날짜를 입력해주세요.');
   }
 
-  return `${inputNumbers.slice(0, 4)}.${inputNumbers.slice(4, 6)}.${inputNumbers.slice(6, 8)}`;
+  return formattedDate;
 };
