b02afc0a09a1d82ef58eaa23f1b093fe9d2e371c
minyeong <minyk0512@gmail.com>
2024-11-20 16:50:52 +0900

    test: 날짜 포맷팅 적용

--- src/utils/date/formatDate.ts
+++ src/utils/date/formatDate.ts
@@ -7,12 +7,12 @@ export const formatDate = (value: string) => {
   if (inputNumbers.length <= 4) return inputNumbers;
   else if (inputNumbers.length <= 6) return `${inputNumbers.slice(0, 4)}.${inputNumbers.slice(4)}`;
 
-  const formattedDate = `${inputNumbers.slice(0, 4)}.${inputNumbers.slice(4, 6)}.${inputNumbers.slice(6, 8)}`;
+  const formattedDate = `${inputNumbers.slice(0, 4)}-${inputNumbers.slice(4, 6)}-${inputNumbers.slice(6, 8)}`;
 
-  const parsedDate = parse(formattedDate, 'yyyy.MM.dd', new Date());
+  const parsedDate = parse(formattedDate, 'yyyy-MM-dd', new Date());
   if (!isValid(parsedDate)) {
     Notify.failure('유효한 날짜를 입력해주세요.');
   }
 
-  return formattedDate;
+  return `${inputNumbers.slice(0, 4)}.${inputNumbers.slice(4, 6)}.${inputNumbers.slice(6, 8)}`;
 };
