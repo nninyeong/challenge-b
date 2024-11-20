487e132d07f76c49c4e32a431b573bd6b43db7d2
minyeong <minyk0512@gmail.com>
2024-11-20 15:35:31 +0900

    test: weddingInfo.date 날짜 유효성검사 위치 변경

--- src/utils/date/formatDate.ts
+++ src/utils/date/formatDate.ts
@@ -1,7 +1,19 @@
+import dayjs from 'dayjs';
+import { Notify } from 'notiflix';
+
 export const formatDate = (value: string) => {
   const inputNumbers = value.replace(/\D/g, '');
 
   if (inputNumbers.length <= 4) return inputNumbers;
   else if (inputNumbers.length <= 6) return `${inputNumbers.slice(0, 4)}.${inputNumbers.slice(4)}`;
-  return `${inputNumbers.slice(0, 4)}.${inputNumbers.slice(4, 6)}.${inputNumbers.slice(6, 8)}`;
+  else {
+    const formattedDate = `${inputNumbers.slice(0, 4)}.${inputNumbers.slice(4, 6)}.${inputNumbers.slice(6, 8)}`;
+    const isValide = dayjs(formattedDate, 'YYYY.MM.DD').isValid();
+    if (!isValide) {
+      Notify.failure('유효한 날짜를 입력해주세요.');
+      return '';
+    }
+
+    return formattedDate;
+  }
 };
